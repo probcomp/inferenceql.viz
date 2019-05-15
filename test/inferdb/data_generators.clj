96d658dbccd226f7724e7bf53e3437ecd27db89b
ulli <ulli@mit.edu>
2019-05-15 14:11:42 -0400

    WIP: add some comments and re-order functions.

--- test/inferdb/data_generators.clj
+++ test/inferdb/data_generators.clj
@@ -1,3 +1,9 @@
+;; The purpose of this file is to provide ground truth solutions; for both
+;; synthetic data and the corresponding quantitities like logPDF, MI, KL, etc.
+
+;; We also want to test certain properties of the function that are supposed to
+;; compute ground truth values.
+
 (ns inferdb.data_generators
   (:refer-clojure :exclude [apply replicate])
   (:require [clojure.pprint :refer [pprint]]
@@ -7,28 +13,8 @@
             [metaprob.distributions :refer :all]
             [inferdb.utils :refer :all]))
 
-(deftest fixed-rand-seed
-  (testing "whether we can fix the rand seed."
-    (let [x (uniform 0 1)
-          y (binding [metaprob.prelude/*rng* (java.util.Random. 42)]
-              (uniform 0 1))]
-    (is (= x y)))))
-
-(def threshold 0.1)
-
-(defn p-gaussian [x mu sigma]
-  (Math/exp (score-gaussian x [mu sigma])))
-
-(deftest check-gaussian-scorer
-  (testing "whether the Gaussian scorer works"
-    (let [true-probability 0.3989422804014327] ;; Computed with scipy stats.
-      (is (almost-equal (p-gaussian 0 0 1) true-probability threshold)))))
-
-
-
 (defn p-gaussian [x parameter]
   (Math/exp (score-gaussian x [(:mu parameter) (:sigma parameter)])))
-
 
 ;; XXX: this only works with a scalar x right now (that get's interpreted as [x x])
 ;; need to do some enumerate shit here...
@@ -39,10 +25,27 @@
                                                  (rest parameter)))))
               parameters)))
 
-(def gmm-parameters
-  [[0.1 {:mu -10 :sigma 1}]
-   [0.8 {:mu   0 :sigma 1}]
-   [0.1 {:mu  10 :sigma 1}]])
+(def threshold 0.0001)
 
-;; Test the above.
-;; (p-gmm 10 gmm-parameters)
+(def gmm-parameters
+  [[0.1 {:mu -10 :sigma 1} {:mu -10 :sigma 1}]
+   [0.8 {:mu   0 :sigma 1} {:mu   0 :sigma 1}]
+   [0.1 {:mu  10 :sigma 1} {:mu  10 :sigma 1}]])
+;; Test the above using a REPL.
+; (p-gmm 10 gmm-parameters)
+
+
+(deftest fixed-rand-seed
+  (testing "whether we can fix the rand seed."
+    (let [x (uniform 0 1)
+          y (binding [metaprob.prelude/*rng* (java.util.Random. 42)]
+              (uniform 0 1))]
+    (is (= x y)))))
+
+(deftest check-gaussian-scorer
+  (testing "whether the Gaussian scorer works"
+    (let [true-probability 0.3989422804014327] ;; Computed with scipy stats.
+      (is (almost-equal (p-gaussian 0 0 1) true-probability threshold)))))
+
+;; TODO: add tests for test harness, test against itself, and test against
+;; fixed, analytical vaule
