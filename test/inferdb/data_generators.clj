0175f1ab9aa08769bc6f5ba0004132c9436f5f9e
ulli <ulli@mit.edu>
2019-05-16 18:32:20 -0400

    Add missing referral.

--- test/inferdb/data_generators.clj
+++ test/inferdb/data_generators.clj
@@ -5,7 +5,7 @@
   We also want to test certain properties of the function that are supposed to
   compute ground truth values. "
   (:refer-clojure :exclude [apply replicate])
-  (:require [clojure.test :refer [deftest testing is]]
+  (:require [clojure.test :refer [deftest testing is use-fixtures]]
             [clojure.string :refer [index-of]]
             [metaprob.distributions :as dist]
             [metaprob.prelude]
