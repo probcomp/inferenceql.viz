9299682b923514abc4aef002a5b5aa7eee9f41d6
ulli <ulli@mit.edu>
2019-05-17 12:59:35 -0400

    Attempt to fix the test runner.
    Tests are run correctly with `make test`. But I don't quite understand why. The
    test runner seems neve to be run. I put an assert there to check this. Weird.

--- test/inferdb/test_runner.clj
+++ test/inferdb/test_runner.clj
@@ -1,7 +1,12 @@
 (ns inferdb.test-runner
-  (:require [inferdb.data-generators :refer :all]))
+  (:require [inferdb.multimixture-test :refer :all])
+  (:require [inferdb.cgpm-test :refer :all])
+  (:require [inferdb.analytical-solutions-mixture-test :refer :all]))
 
 (defn -main
   [& args]
   (binding [metaprob.prelude/*rng* (java.util.Random. 42)]
-  (clojure.test/run-tests 'inferdb.data-generators)))
+  (assert false "I am never called!?")
+  (clojure.test/run-tests 'inferdb.multimixture-test)
+  (clojure.test/run-tests 'inferdb.cgpm-test)
+  (clojure.test/run-tests 'inferdb.analytical-solutions-mixture-test)))
