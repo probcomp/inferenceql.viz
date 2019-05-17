d29e4622b020c80271828d65acd37056d14b9132
ulli <ulli@mit.edu>
2019-05-17 11:21:54 -0400

    Add support to compute two vectors.

--- test/inferdb/utils.clj
+++ test/inferdb/utils.clj
@@ -1,12 +1,23 @@
 (ns inferdb.utils)
 
 
-(defn abs
-  [n]
+(defn abs [n]
   (max n (- n)))
 
+(defn all? [l]
+  (every? (fn [x] x) l))
+
 (defn almost-equal
-  "Returns true if `a` and `b` are approximately equal. Takes a difference
+  "Returns true if scalars `a` and `b` are approximately equal. Takes a difference
   metric (presumably from `inferdb.metrics`) as its second argument."
   [a b difference-metric threshold]
   (< (difference-metric a b) threshold))
+
+(defn almost-equal-vectors
+  "Returns true if vectors `a` and `b` are approximately equal. Takes a difference
+  metric (presumably from `inferdb.metrics`) as its second argument."
+  [a b difference-metric threshold]
+  (assert (count a) (count b))
+  (let [call-almost-equal
+        (fn [i] (almost-equal  (nth a i) (nth b i) difference-metric threshold))]
+  (all? (map call-almost-equal (range (count a))))))
