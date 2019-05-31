508bdda661ae4a690626ac46dbb0200789c49bb3
Zane Shelby <zane@users.noreply.github.com>
2019-05-31 03:10:13 -0400

    Remove unnecessary test/inferdb/charts namespaces

--- test/inferdb/charts/simulations_b.clj
+++
@@ -1,9 +0,0 @@
-(ns inferdb.charts.simulations-b
-  (:require [clojure.walk :as walk]
-            [cheshire.core :as cheshire]
-            [inferdb.cgpm.main :as cgpm]
-            [clojure.repl :refer [doc source]]
-            [clojure.test :refer :all]
-            [clojure.string :refer [index-of]]))
-
-(defn -main [] (println (slurp "out/json-results/simulations-b.json")))
