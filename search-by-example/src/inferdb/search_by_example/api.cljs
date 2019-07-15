38ab54babe8365e949a8b4c178b5b0989dc29d96
Zane Shelby <zane@users.noreply.github.com>
2019-07-14 20:52:16 -0400

    Add data and usage examples

--- search-by-example/src/inferdb/search_by_example/api.cljs
+++ search-by-example/src/inferdb/search_by_example/api.cljs
@@ -1,5 +1,8 @@
 (ns inferdb.search-by-example.api
-  (:require [inferdb.spreadsheets.search :as search]))
+  (:require [inferdb.spreadsheets.data :as data]
+            [inferdb.spreadsheets.search :as search]))
+
+(def ^:export data (clj->js data/nyt-data))
 
 (defn ^:export search
   [example emphasis]
@@ -7,3 +10,10 @@
    (search/search-by-example (js->clj example)
                              (js->clj emphasis)
                              1)))
+
+(defn ^:export isVeryAnomalous
+  [example]
+  (search/very-anomalous? (reduce-kv (fn [m k v]
+                                       (assoc m (keyword k) v))
+                                     {}
+                                     (js->clj example))))
