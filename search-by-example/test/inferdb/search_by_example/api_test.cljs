11943f3cea5ddc6497f2ace62b3c9645e7f354f0
Zane Shelby <zane@users.noreply.github.com>
2019-07-17 16:59:58 -0400

    More tests

--- search-by-example/test/inferdb/search_by_example/api_test.cljs
+++ search-by-example/test/inferdb/search_by_example/api_test.cljs
@@ -4,9 +4,26 @@
 
 (deftest validation-errors
   (let [validation-errors #'api/validation-errors]
-    (is (empty? (validation-errors {})))
-    (is (empty? (validation-errors {"percent_married" 0.01})))
-    (is (not-empty (validation-errors {"invalid_column" 0.01})))))
+    (testing "valid"
+      (is (empty? (validation-errors {})))
+      (is (empty? (validation-errors {"percent_married" 0.1}))))
+    (testing "invalid column"
+      (let [[error :as errors] (validation-errors {"invalid_column" 0.1})]
+        (is (= 1 (count errors)))
+        (is (= :invalid-column (:type error)))))
+    (testing "invalid value"
+      (testing "numeric"
+        (let [[error :as errors] (validation-errors {"percent_married" "invalid numeric value"})]
+          (is (= 1 (count errors)))
+          (is (= :invalid-value (:type error)))))
+      (testing "nominal"
+        (let [[error :as errors] (validation-errors {"nyt_political_prediction" 0.1})]
+          (is (= 1 (count errors)))
+          (is (= :invalid-value (:type error))))))
+    (testing "invalid column and value"
+      (let [errors (validation-errors {"percent_married" "invalid numeric value"
+                                       "nyt_political_prediction" 0.1})]
+        (is (= 2 (count errors)))))))
 
 (deftest validation
   (doseq [f #{api/isVeryAnomalous api/search}]
