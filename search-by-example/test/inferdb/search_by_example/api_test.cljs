b6890b15e6806e9393ec38ec676a46a04709a806
Zane Shelby <zane@users.noreply.github.com>
2019-07-17 17:05:25 -0400

    DRY tests

--- search-by-example/test/inferdb/search_by_example/api_test.cljs
+++ search-by-example/test/inferdb/search_by_example/api_test.cljs
@@ -2,33 +2,37 @@
   (:require [clojure.test :as test :refer [are deftest is testing]]
             [inferdb.search-by-example.api :as api]))
 
+(def invalid-column {"invalid_column" 0.1})
+(def invalid-numeric-value {"percent_married" "invalid numeric value"})
+(def invalid-nominal-value {"nyt_political_prediction" 0.1})
+(def invalid-examples #{invalid-column invalid-numeric-value invalid-nominal-value})
+
 (deftest validation-errors
   (let [validation-errors #'api/validation-errors]
     (testing "valid"
       (is (empty? (validation-errors {})))
       (is (empty? (validation-errors {"percent_married" 0.1}))))
     (testing "invalid column"
-      (let [[error :as errors] (validation-errors {"invalid_column" 0.1})]
+      (let [[error :as errors] (validation-errors invalid-column)]
         (is (= 1 (count errors)))
         (is (= :invalid-column (:type error)))))
     (testing "invalid value"
       (testing "numeric"
-        (let [[error :as errors] (validation-errors {"percent_married" "invalid numeric value"})]
+        (let [[error :as errors] (validation-errors invalid-numeric-value)]
           (is (= 1 (count errors)))
           (is (= :invalid-value (:type error)))))
       (testing "nominal"
-        (let [[error :as errors] (validation-errors {"nyt_political_prediction" 0.1})]
+        (let [[error :as errors] (validation-errors invalid-nominal-value)]
           (is (= 1 (count errors)))
           (is (= :invalid-value (:type error))))))
-    (testing "invalid column and value"
-      (let [errors (validation-errors {"percent_married" "invalid numeric value"
-                                       "nyt_political_prediction" 0.1})]
-        (is (= 2 (count errors)))))))
+    (testing "multiple validation issues"
+      (let [errors (validation-errors (merge invalid-column
+                                             invalid-numeric-value
+                                             invalid-nominal-value))]
+        (is (= 3 (count errors)))))))
 
 (deftest validation
   (doseq [f #{api/isVeryAnomalous api/search}]
     (testing f
-      (are [example] (thrown? js/Error. (f example))
-        #js {"invalid_column" 0.01}
-        #js {"percent_married" "invalid numeric value"}
-        #js {"nyt_political_prediction" 0.01}))))
+      (doseq [example invalid-examples]
+        (is (thrown? js/Error. (f (clj->js example))))))))
