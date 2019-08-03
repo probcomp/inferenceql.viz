07f6076e62c93e0252d46b72a0c023d365b10d30
Zane Shelby <zane@users.noreply.github.com>
2019-08-03 12:07:05 -0400

    Extract out search into separate namespace

--- multimixture/src/inferdb/multimixture.clj
+++ multimixture/src/inferdb/multimixture.clj
@@ -2,11 +2,7 @@
   (:require [clojure.math.combinatorics :as combo]
             [metaprob.generative-functions :as gfn :refer [apply-at at gen]]
             [metaprob.distributions :as dist]
-            [metaprob.prelude :as mp]
-            [metaprob.inference :as inference]
-            [inferdb.multimixture.dsl-test :as dsl-test]
-            [inferdb.multimixture.specification :as spec]
-            [zane.vega.repl :as vega]))
+            [metaprob.prelude :as mp]))
 
 #_(def mmix
     [{:vars {"x" :gaussian
@@ -23,6 +19,11 @@
                   :parameters {"a" [6 7]
                                "b" [[0.99 0.001 0.002 0.003 0.004]]}}]}])
 
+;; This is a duplicate of inferdb.multimixture.specification/view-variables,
+;; only it doesn't keywordize the results before returning them. Probably the
+;; right thing to do is to change that version to not keywordize variables
+;; either, delete this one, and then have the callers who depend on the results
+;; being keywords do that conversion themselves.
 (defn view-variables
   "Returns the variables assigned to given view."
   [view]
@@ -96,39 +97,9 @@
 
 (declare optimized-row-generator)
 
-(defn- uniform-categorical-params
+(defn uniform-categorical-params
   [n]
   (repeat n (double (/ 1 n))))
-
-(def generate-1col-binary-extension
-  (gen [mmix-spec row-count column-key {:keys [alpha beta]}]
-    (let [view-idx (at :view dist/categorical (uniform-categorical-params (count mmix-spec)))
-          new-spec (-> mmix-spec
-                       (assoc-in [view-idx :vars column-key] :binary)
-                       (update-in [view-idx :clusters]
-                                  (fn [clusters]
-                                    (vec (map-indexed (fn [i cluster]
-                                                        (update cluster :parameters
-                                                                #(assoc % column-key
-                                                                        [(at `(:cluster-parameters ~i)
-                                                                             dist/beta alpha beta)])))
-                                                      clusters)))))
-          new-row-generator (optimized-row-generator new-spec)]
-      (doseq [i (range row-count)]
-        (at `(:rows ~i) new-row-generator))
-      new-spec)))
-
-#_(let [rows [{"x" 0, "y" true}
-              {"x" 5, "y" false}]
-        spec [{:vars {"x" :gaussian}
-               :clusters [{:probability 0.75
-                           :parameters {"x" [0 1]}}
-                          {:probability 0.25
-                           :parameters {"x" [5 1]}}]}]]
-    #_
-    (mp/infer-and-score :procedure (optimized-row-generator spec))
-    (mp/infer-and-score :procedure generate-1col-binary-extension
-                        :inputs [spec (count rows) "y" {:alpha 0.001 :beta 0.001}]))
 
 (defn with-rows
   "Given a trace for generate-1col, produces a trace with the values in rows
@@ -151,67 +122,6 @@
                                                       :observation-trace observation-trace)))]
     (nth particles (dist/log-categorical (map (fn [[_ _ s]] s) particles)))))
 
-(defn insert-column
-  "Takes a multimixture specification, views, and a set of rows that have a value
-  in the new column that is being added. Returns an updated multimixture
-  specification."
-  [spec rows column-key beta-params]
-  (first
-   (importance-resampling :model generate-1col-binary-extension
-                          :inputs [spec (count rows) column-key beta-params]
-                          :observation-trace (with-rows {} rows)
-                          :n-particles 100)))
-
-(defn score-rows
-  [spec rows new-column-key]
-  (let [new-column-view (spec/view-index-for-variable spec new-column-key)
-        row-generator (optimized-row-generator spec)]
-    (mapv (fn [row]
-            (let [[_ trace _] (mp/infer-and-score :procedure row-generator
-                                                  :observation-trace (with-row-values {} row))
-                  cluster-idx (get-in trace [:cluster-assignments-for-view new-column-view :value])]
-              (get-in spec [new-column-view :clusters cluster-idx :parameters new-column-key 0])))
-          rows)))
-
-#_(let [rows [{"x" 0}
-              {"x" 5}]
-        spec [{:vars {"x" :gaussian
-                      "y" :binary}
-               :clusters [{:probability 0.75
-                           :parameters {"x" [0 1]
-                                        "y" [0]}}
-                          {:probability 0.25
-                           :parameters {"x" [5 1]
-                                        "y" [1]}}]}]]
-    (score-rows spec rows "y"))
-
-(defn search
-  [spec new-column-key known-rows unknown-rows n-models beta-params]
-  (let [specs (repeatedly n-models #(insert-column spec known-rows new-column-key beta-params))
-        scores (map (fn [spec]
-                      (score-rows spec unknown-rows new-column-key))
-                    specs)]
-    (mapv (fn [i]
-            (/ (reduce + (map #(nth % i) scores))
-               n-models))
-          (range (count unknown-rows)))))
-
-#_(let [unknown-rows [{"x" 0}
-                      {"x" 1}
-                      {"x" 2}
-                      {"x" 3}
-                      {"x" 4}
-                      {"x" 5}]
-        known-rows [{"x" 0 "y" true}
-                    {"x" 0 "y" true}
-                    {"x" 5 "y" false}]
-        spec [{:vars {"x" :gaussian}
-               :clusters [{:probability 0.5
-                           :parameters {"x" [0 2]}}
-                          {:probability 0.5
-                           :parameters {"x" [5 2]}}]}]]
-    (time (search spec "y" known-rows unknown-rows 100 {:alpha 0.001 :beta 0.001})))
-
 (defn- valueify
   [m]
   (reduce-kv (fn [m k v]
@@ -225,9 +135,9 @@
   (->> spec
        (map (comp range count :clusters))
        (apply combo/cartesian-product)
-       (map (fn [cluster-assignments]
+       (map (fn [assignments]
               {:cluster-assignments-for-view
-               (->> cluster-assignments
+               (->> assignments
                     (map-indexed (fn [view-i cluster-i]
                                    {view-i {:value cluster-i}}))
                     (into {}))}))))
@@ -245,36 +155,3 @@
       (dist/normalize-numbers scores))))
 
 #_(score-probabilities (map #(Math/log %) [1 2 3 0]))
-
-(defn optimized-row-generator
-  [spec]
-  (let [row-generator (row-generator spec)]
-    (gfn/make-generative-function
-     row-generator
-     (gen [partial-trace]
-       (let [all-latents    (all-latents spec)
-             all-traces     (mapv #(merge partial-trace %)
-                                  all-latents)
-             all-logscores  (mapv #(last (mp/infer-and-score :procedure row-generator
-                                                             :observation-trace %))
-                                  all-traces)
-             all-scores (map mp/exp all-logscores)
-             all-zeroes (every? #(== 0 %) all-scores)
-             log-normalizer (if all-zeroes ##-Inf (dist/logsumexp all-logscores))
-             score          log-normalizer
-             categorical-params (if all-zeroes
-                                  (uniform-categorical-params (count all-scores))
-                                  (dist/normalize-numbers all-scores))]
-         (gen []
-           (let [i     (dist/categorical categorical-params)
-                 trace (nth all-traces i)
-                 v     (first (mp/infer-and-score :procedure row-generator
-                                                  :observation-trace trace))]
-             [v trace score])))))))
-
-#_(dist/logsumexp [##-Inf])
-#_(dist/normalize-numbers [0 0 0])
-
-#_(optimized-row-generator dsl-test/multi-mixture)
-#_((optimized-row-generator dsl-test/multi-mixture))
-#_(repeatedly 100 #(mp/infer-and-score :procedure (optimized-row-generator dsl-test/multi-mixture)))
---
+++ multimixture/src/inferdb/multimixture/search.clj
@@ -0,0 +1,128 @@
+(ns inferdb.multimixture.search
+  (:require [metaprob.distributions :as dist]
+            [metaprob.generative-functions :as g :refer [at gen]]
+            [metaprob.prelude :as mp]
+            [inferdb.multimixture :as mmix]
+            [inferdb.multimixture.specification :as spec]))
+
+(defn optimized-row-generator
+  [spec]
+  (let [row-generator (mmix/row-generator spec)]
+    (g/make-generative-function
+     row-generator
+     (gen [partial-trace]
+       (let [all-latents    (mmix/all-latents spec)
+             all-traces     (mapv #(merge partial-trace %)
+                                  all-latents)
+             all-logscores  (mapv #(last (mp/infer-and-score :procedure row-generator
+                                                             :observation-trace %))
+                                  all-traces)
+             all-scores (map mp/exp all-logscores)
+             all-zeroes (every? #(== 0 %) all-scores)
+             log-normalizer (if all-zeroes ##-Inf (dist/logsumexp all-logscores))
+             score          log-normalizer
+             categorical-params (if all-zeroes
+                                  (mmix/uniform-categorical-params (count all-scores))
+                                  (dist/normalize-numbers all-scores))]
+         (gen []
+           (let [i     (dist/categorical categorical-params)
+                 trace (nth all-traces i)
+                 v     (first (mp/infer-and-score :procedure row-generator
+                                                  :observation-trace trace))]
+             [v trace score])))))))
+
+#_(require '[inferdb.multimixture.dsl-test :as dsl-test])
+#_(optimized-row-generator dsl-test/multi-mixture)
+#_((optimized-row-generator dsl-test/multi-mixture))
+#_(repeatedly 100 #(mp/infer-and-score :procedure (optimized-row-generator dsl-test/multi-mixture)))
+
+(def generate-1col-binary-extension
+  (gen [spec row-count column-key {:keys [alpha beta]}]
+    (let [view-idx (at :view dist/categorical (mmix/uniform-categorical-params (count spec)))
+          new-spec (-> spec
+                       (assoc-in [view-idx :vars column-key] :binary)
+                       (update-in [view-idx :clusters]
+                                  (fn [clusters]
+                                    (vec (map-indexed (fn [i cluster]
+                                                        (update cluster :parameters
+                                                                #(assoc % column-key
+                                                                        [(at `(:cluster-parameters ~i)
+                                                                             dist/beta alpha beta)])))
+                                                      clusters)))))
+          new-row-generator (optimized-row-generator new-spec)]
+      (doseq [i (range row-count)]
+        (at `(:rows ~i) new-row-generator))
+      new-spec)))
+
+#_(let [rows [{"x" 0, "y" true}
+              {"x" 5, "y" false}]
+        spec [{:vars {"x" :gaussian}
+               :clusters [{:probability 0.75
+                           :parameters {"x" [0 1]}}
+                          {:probability 0.25
+                           :parameters {"x" [5 1]}}]}]]
+    #_
+    (mp/infer-and-score :procedure (optimized-row-generator spec))
+    (mp/infer-and-score :procedure generate-1col-binary-extension
+                        :inputs [spec (count rows) "y" {:alpha 0.001 :beta 0.001}]))
+
+(defn insert-column
+  "Takes a multimixture specification, views, and a set of rows that have a value
+  in the new column that is being added. Returns an updated multimixture
+  specification."
+  [spec rows column-key beta-params]
+  (first
+   ;; TODO: Setting n-particles to 1 causes IOB errors
+   (mmix/importance-resampling :model generate-1col-binary-extension
+                               :inputs [spec (count rows) column-key beta-params]
+                               :observation-trace (mmix/with-rows {} rows)
+                               :n-particles 10)))
+
+(defn score-rows
+  [spec rows new-column-key]
+  (let [new-column-view (spec/view-index-for-variable spec new-column-key)
+        row-generator (optimized-row-generator spec)]
+    (mapv (fn [row]
+            (let [[_ trace _] (mp/infer-and-score :procedure row-generator
+                                                  :observation-trace (mmix/with-row-values {} row))
+                  cluster-idx (get-in trace [:cluster-assignments-for-view new-column-view :value])]
+              (get-in spec [new-column-view :clusters cluster-idx :parameters new-column-key 0])))
+          rows)))
+
+#_(let [rows [{"x" 0}
+              {"x" 5}]
+        spec [{:vars {"x" :gaussian
+                      "y" :binary}
+               :clusters [{:probability 0.75
+                           :parameters {"x" [0 1]
+                                        "y" [0]}}
+                          {:probability 0.25
+                           :parameters {"x" [5 1]
+                                        "y" [1]}}]}]]
+    (score-rows spec rows "y"))
+
+(defn search
+  [spec new-column-key known-rows unknown-rows n-models beta-params]
+  (let [specs (repeatedly n-models #(insert-column spec known-rows new-column-key beta-params))
+        scores (map (fn [spec]
+                      (score-rows spec unknown-rows new-column-key))
+                    specs)]
+    (mapv (fn [i]
+            (/ (reduce + (map #(nth % i) scores))
+               n-models))
+          (range (count unknown-rows)))))
+
+#_(let [unknown-rows (repeat 100 {"x" 0})
+        known-rows [{"x" 0 "y" true}
+                    {"x" 5 "y" false}]
+        spec [{:vars {"x" :gaussian}
+               :clusters [{:probability 0.5
+                           :parameters {"x" [0 2]}}
+                          {:probability 0.5
+                           :parameters {"x" [5 2]}}]}
+              {:vars {"z" :gaussian}
+               :clusters [{:probability 0.5
+                           :parameters {"z" [0 2]}}
+                          {:probability 0.5
+                           :parameters {"z" [5 2]}}]}]]
+    (time (search spec "y" known-rows unknown-rows 10 {:alpha 0.001 :beta 0.001})))
