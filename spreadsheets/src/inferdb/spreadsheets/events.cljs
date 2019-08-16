0381cf835f02cc72a21a6046773df7b8d64cbb1e
Harish Tella <htella@mit.edu>
2019-08-16 11:48:59 -0400

    Change command for scoring by labels to be more similar to conditional searches -- again

--- spreadsheets/src/inferdb/spreadsheets/events.cljs
+++ spreadsheets/src/inferdb/spreadsheets/events.cljs
@@ -126,7 +126,7 @@
 
 (defn score-using-labels-statement?
   [text]
-  (not (nil? (re-matches #"SCORE PROBABILITY OF LABEL=\"True\" GIVEN ROW" text))))
+  (not (nil? (re-matches #"SCORE PROBABILITY OF label=\"True\" GIVEN ROW" text))))
 
 (rf/reg-event-db
  :search
