c6d650cc32e1995509871225d508da748bda6f42
Harish Tella <htella@mit.edu>
2019-09-06 18:15:23 -0400

    Change default value for confidence threshold

--- spreadsheets/src/inferdb/spreadsheets/db.cljs
+++ spreadsheets/src/inferdb/spreadsheets/db.cljs
@@ -109,5 +109,5 @@
    ::rows nyt-data
    ::virtual-rows []
    :hot-state {:real-table nil :virtual-table nil}
-   ::confidence-threshold 0.3
+   ::confidence-threshold 0.9
    ::confidence-options {:mode :none}})
