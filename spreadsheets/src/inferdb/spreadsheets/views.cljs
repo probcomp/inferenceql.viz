3d81d270b4ea4c9c92dcecd6f75739ecabcdba66
Harish Tella <htella@mit.edu>
2019-09-30 10:29:17 -0400

    Add better settings to handsontable (#124)
    * Enable column resizing
    * Enable column filtering
    * Prevent wrap-around when using arrow keys to move between cells at the
    edges of the table.

--- spreadsheets/src/inferdb/spreadsheets/views.cljs
+++ spreadsheets/src/inferdb/spreadsheets/views.cljs
@@ -14,7 +14,18 @@
               :multiColumnSorting  true
               :manualColumnMove    true
               :beforeColumnMove    hot/freeze-col-1-2-fn
+              :manualColumnResize  true
+              :autoWrapCol         false
+              :autoWrapRow         false
               :filters             true
+              ;; TODO: investigate more closely what each of
+              ;; these options adds. And if they can be put
+              ;; in the context-menu instead.
+              :dropdownMenu        ["filter_by_condition"
+                                    "filter_operators"
+                                    "filter_by_condition2"
+                                    "filter_by_value"
+                                    "filter_action_bar"]
               :bindRowsWithHeaders true
               :selectionMode       :range
               :outsideClickDeselects false
