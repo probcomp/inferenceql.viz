76027d4be5a4aae2c4baa8d117397eea89fefba3
Harish Tella <htella@mit.edu>
2019-10-31 18:23:06 -0400

    Make small improvements in code clarity. (#172)
    * Make use of reduce-kv in column_overrides.
    * Create a predicate function in vega-lite-spec sub.

--- spreadsheets/src/inferenceql/spreadsheets/column_overrides.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/column_overrides.cljs
@@ -7,12 +7,10 @@
   dependencies, so we can apply them in any order."
   [override-map]
   (fn [row]
-    (loop [[[oride-col oride-fn] & orides-rem] override-map
-           row-state row]
-      (if (and oride-col oride-fn)
-        (let [deps (js/getArgs oride-fn)
-              dep-vals (map #(get row-state %) deps)
-              new-val (apply oride-fn dep-vals)
-              new-row-state (assoc row-state oride-col new-val)]
-          (recur orides-rem new-row-state))
-        row-state))))
+    (reduce-kv (fn [row oride-col oride-fn]
+                 (let [func-deps (js/getArgs oride-fn)
+                       dep-vals (map #(get row %) func-deps)
+                       new-col-val (apply oride-fn dep-vals)]
+                   (assoc row oride-col new-col-val)))
+               row
+               override-map)))
