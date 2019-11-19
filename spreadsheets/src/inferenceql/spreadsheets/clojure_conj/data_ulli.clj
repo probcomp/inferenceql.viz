db531d342226065a633c5d2ad328ec7f0d894670
Harish Tella <htella@mit.edu>
2019-11-19 14:09:44 -0500

    Add code to read in partition info given by Ulli

---
+++ spreadsheets/src/inferenceql/spreadsheets/clojure_conj/data_ulli.clj
@@ -0,0 +1,45 @@
+(ns inferenceql.spreadsheets.clojure-conj.data-ulli
+  (:require
+    [inferenceql.spreadsheets.data :refer [fix-row csv-data->maps]]
+    [clojure.data.csv :as csv]
+    [clojure.java.io :as io]
+    [clojure.string :as str]
+    [medley.core :as medley]
+    [clojure.data.json :as json]))
+
+(def data-filename "stackoverflow-demo-subset-v4.csv")
+(def partitions-filename "partitions.json")
+
+;----------------------------
+
+(def csv-lines (-> data-filename (io/resource) (slurp) (csv/read-csv) (vec)))
+(def so-data (->> csv-lines
+                  (mapv fix-row)
+                  (csv-data->maps)
+                  (map (fn [id row] (assoc row "id" id)) (range))))
+
+(def column-mapping (zipmap (range) (first csv-lines)))
+
+;----------------------------
+
+(def partition-data (-> partitions-filename (io/resource) (slurp) (json/read-str)))
+
+(def model-1 (get partition-data "0"))
+(def model-1-iter (get model-1 "9"))
+
+(def col-view-assignments (->> (get model-1-iter "view-partitions")
+                               (medley/map-keys #(Integer/parseInt %))
+                               (medley/map-keys #(get column-mapping %))))
+(def views (get model-1-iter "view-row-partitions"))
+
+(def view-ids (keys views))
+(def cluster-ids
+  (medley/map-vals #(vec (distinct %)) views))
+
+(defn assign-partitions-to-rows [rows views]
+  (let [add-view-info (fn [rows view-id cluster-assignments]
+                        (let [view-name (str "view-" view-id)]
+                          (map (fn [row c-assignment] (assoc row view-name c-assignment)) rows cluster-assignments)))]
+    (reduce-kv add-view-info rows views)))
+
+(def clustered-so-data (assign-partitions-to-rows so-data views))
