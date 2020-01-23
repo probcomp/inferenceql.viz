3c3ba17ae7a4dd975d00b93ad3ee8ea31c9e8b26
Harish Tella <htella@mit.edu>
2020-01-23 12:02:54 -0500

    Add comments to require statements in core.cljs

--- spreadsheets/src/inferenceql/spreadsheets/core.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/core.cljs
@@ -2,12 +2,16 @@
   (:require [goog.dom :as dom]
             [re-frame.core :as rf]
             [reagent.core :as reagent]
+            ;; Core
             [inferenceql.spreadsheets.events]
             [inferenceql.spreadsheets.subs]
             [inferenceql.spreadsheets.views :as views]
+            ;; Control Panel
             [inferenceql.spreadsheets.panels.control.events]
             [inferenceql.spreadsheets.panels.control.subs]
+            ;; Viz Panel
             [inferenceql.spreadsheets.panels.viz.subs]
+            ;; Library functions for user-defined JS functions.
             [inferenceql.user]))
 
 (enable-console-print!)
