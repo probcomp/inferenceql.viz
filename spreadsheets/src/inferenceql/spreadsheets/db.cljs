7d284c561967fe233231c63cda701e3939c05252
Harish Tella <htella@mit.edu>
2020-01-24 11:25:55 -0500

    Clean up merging of db's

--- spreadsheets/src/inferenceql/spreadsheets/db.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/db.cljs
@@ -142,11 +142,19 @@
   [db]
   (get-in db [::labels]))
 
+(def default-general-db
+  "This db map is meant to contain keys and values not specific to
+  any application panel."
+  {::headers (into [] (keys (first nyt-data)))
+   ::rows nyt-data
+   ::virtual-rows []
+   ::hot-state {:real-table nil :virtual-table nil}})
+
 (defn default-db
-  "When the application starts, this will be the value put in `app-db`."
+  "When the application starts, this will be the value put in `app-db`.
+  It consists of keys and values from the general db
+  and panel specific dbs all merged together."
   []
-  (-> {::headers (into [] (keys (first nyt-data)))
-       ::rows nyt-data
-       ::virtual-rows []
-       ::hot-state {:real-table nil :virtual-table nil}}
-      (merge control-panel/default-db)))
+  (let [dbs [default-general-db
+             control-panel/default-db]]
+    (apply merge dbs)))
