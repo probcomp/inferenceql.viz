bdef5116bc05b69a0197dc0060316b00832d1c89
Harish Tella <htella@mit.edu>
2020-01-24 14:37:56 -0500

    Move subs for table-panel.
    Moving subs for the table panel into panels/table/subs.cljs

--- spreadsheets/src/inferenceql/spreadsheets/panels/table/subs.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/panels/table/subs.cljs
@@ -1,1 +1,283 @@
-(ns inferenceql.spreadsheets.panels.table.subs)
+(ns inferenceql.spreadsheets.panels.table.subs
+  (:require [clojure.string :as str]
+            [re-frame.core :as rf]
+            [inferenceql.spreadsheets.db :as db]
+            [inferenceql.spreadsheets.panels.viz.vega :as vega]
+            [inferenceql.spreadsheets.renderers :as rends]
+            [inferenceql.spreadsheets.panels.table.handsontable :as hot]
+            [inferenceql.spreadsheets.modal :as modal]))
+
+(rf/reg-sub :labels
+            (fn [db _]
+              (db/labels db)))
+
+(def clean-label
+  "Prepares the user-typed label for checking."
+  (fnil (comp str/upper-case str/trim) ""))
+
+(defn- pos-label? [label-str]
+  (let [f (clean-label label-str)]
+    ;; TODO: add more truthy values
+    (or (= f "TRUE")
+        (= f "1"))))
+
+(defn- neg-label? [label-str]
+  (let [f (clean-label label-str)]
+    ;; TODO: add more falsey values
+    (or (= f "FALSE")
+        (= f "0"))))
+
+(defn- unlabeled? [label-str]
+  (and (not (pos-label? label-str))
+       (not (neg-label? label-str))))
+
+(defn row-ids-labeled-pos
+  [{:keys [labels]} _]
+  (let [labels-with-ids (map vector labels (range))
+        ids (->> (filter #(pos-label? (first %)) labels-with-ids)
+                 (map second))]
+    ids))
+(rf/reg-sub :row-ids-labeled-pos
+            (fn [_ _]
+              {:labels (rf/subscribe [:labels])})
+            row-ids-labeled-pos)
+
+(defn row-ids-labeled-neg
+  [{:keys [labels]} _]
+  (let [labels-with-ids (map vector labels (range))
+        ids (->> (filter #(neg-label? (first %)) labels-with-ids)
+                 (map second))]
+    ids))
+(rf/reg-sub :row-ids-labeled-neg
+            (fn [_ _]
+              {:labels (rf/subscribe [:labels])})
+            row-ids-labeled-neg)
+
+(defn row-ids-unlabeled
+  [{:keys [labels]} _]
+  (let [labels-with-ids (map vector labels (range))
+        ids (->> (filter #(unlabeled? (first %)) labels-with-ids)
+                 (map second))]
+    ids))
+(rf/reg-sub :row-ids-unlabeled
+            (fn [_ _]
+              {:labels (rf/subscribe [:labels])})
+            row-ids-unlabeled)
+
+(rf/reg-sub :one-cell-selected
+            (fn [_ _]
+              (rf/subscribe [:table-state-active]))
+            (fn [{:keys [selections]}]
+              (= 1
+                 (count selections)
+                 (count (first selections))
+                 (count (keys (first selections))))))
+
+(defn table-headers
+  [db _]
+  (db/table-headers db))
+(rf/reg-sub :table-headers table-headers)
+
+(rf/reg-sub :table-last-clicked
+            (fn [db _]
+              (get db ::db/table-last-clicked)))
+
+(rf/reg-sub :other-table
+            (fn [db [_sub-name table-picked]]
+              (let [[table-1-id table-2-id] (keys (get db ::db/hot-state))]
+                (condp = table-picked
+                  table-1-id table-2-id
+                  table-2-id table-1-id))))
+
+(rf/reg-sub :table-state
+            (fn [db [_sub-name table-id]]
+              (get-in db [::db/hot-state table-id])))
+
+(rf/reg-sub :both-table-states
+            (fn [db [_sub-name]]
+              (get db ::db/hot-state)))
+
+(rf/reg-sub :table-state-active
+            (fn [_ _]
+              {:table-id (rf/subscribe [:table-last-clicked])
+               :table-states (rf/subscribe [:both-table-states])})
+            (fn [{:keys [table-id table-states]}]
+              (get table-states table-id)))
+
+(rf/reg-sub :computed-headers
+            (fn [_ _]
+              (rf/subscribe [:table-headers]))
+            (fn [headers]
+              (into [vega/label-col-header vega/score-col-header] headers)))
+
+(rf/reg-sub :computed-rows
+            (fn [_ _]
+              {:rows (rf/subscribe [:table-rows])
+               :scores (rf/subscribe [:scores])
+               :labels (rf/subscribe [:labels])
+               :imputed-values (rf/subscribe [:missing-cells-vals-above-thresh])
+               :conf-mode (rf/subscribe [:confidence-option [:mode]])})
+            (fn [{:keys [rows scores labels imputed-values conf-mode]}]
+              (let [merge-imputed (and (= conf-mode :cells-missing)
+                                       (seq imputed-values))]
+                (cond->> rows
+                  merge-imputed (mapv (fn [imputed-values-in-row row]
+                                        (merge row imputed-values-in-row))
+                                      imputed-values)
+                  scores (mapv (fn [score row]
+                                 (assoc row vega/score-col-header score))
+                               scores)
+                  labels (mapv (fn [label row]
+                                 (assoc row vega/label-col-header label))
+                               labels)))))
+
+(rf/reg-sub :virtual-computed-rows
+  (fn [_ _]
+    {:rows (rf/subscribe [:virtual-rows])
+     :scores (rf/subscribe [:virtual-scores])})
+  (fn [{:keys [rows scores]}]
+    (let [num-missing-scores (- (count rows) (count scores))
+          dummy-scores (repeat num-missing-scores nil)
+          scores (concat dummy-scores scores)]
+
+      ;; Creation of dummy scores allows correct attaching of old scores to
+      ;; rows even when new rows are generated after a scoring event.
+      (mapv (fn [score row] (assoc row vega/score-col-header score))
+            scores rows))))
+
+(defn table-rows
+  [db _]
+  (db/table-rows db))
+(rf/reg-sub :table-rows table-rows)
+
+(defn virtual-rows
+  [db _]
+  (db/virtual-rows db))
+(rf/reg-sub :virtual-rows virtual-rows)
+
+(defn- cell-vector
+  "Takes tabular data represented as a sequence of maps and reshapes the data as a
+  2D vector of cells and a vector of headers."
+  [headers rows]
+  (into []
+        (map (fn [row]
+               (into []
+                     (map #(get row %))
+                     headers)))
+        rows))
+
+(defn real-hot-props
+  [{:keys [headers rows cells-style-fn context-menu]} _]
+  (let [data (cell-vector headers rows)
+
+        num-columns (count headers)
+        initial-column-setting {:readOnly false}
+        rem-column-settings (repeat (dec num-columns) {})
+
+        all-column-settings (cons initial-column-setting rem-column-settings)]
+    (-> hot/real-hot-settings
+        (assoc-in [:settings :data] data)
+        (assoc-in [:settings :colHeaders] headers)
+        (assoc-in [:settings :columns] all-column-settings)
+        (assoc-in [:settings :cells] cells-style-fn)
+        (assoc-in [:settings :contextMenu] context-menu))))
+(rf/reg-sub :real-hot-props
+            (fn [_ _]
+              {:headers (rf/subscribe [:computed-headers])
+               :rows    (rf/subscribe [:computed-rows])
+               :cells-style-fn (rf/subscribe [:cells-style-fn])
+               :context-menu (rf/subscribe [:context-menu])})
+            real-hot-props)
+
+(defn virtual-hot-props
+  [{:keys [headers rows]} _]
+  (let [data (cell-vector headers rows)
+        num-columns (count headers)
+        column-settings (repeat num-columns {})]
+    (-> hot/virtual-hot-settings
+        (assoc-in [:settings :data] data)
+        (assoc-in [:settings :colHeaders] headers)
+        (assoc-in [:settings :columns] column-settings))))
+(rf/reg-sub :virtual-hot-props
+            (fn [_ _]
+              {:headers (rf/subscribe [:computed-headers])
+               :rows    (rf/subscribe [:virtual-computed-rows])})
+            virtual-hot-props)
+
+(rf/reg-sub
+ :context-menu
+ (fn [_ _]
+   {:col-overrides (rf/subscribe [:column-overrides])
+    :col-names (rf/subscribe [:computed-headers])})
+ (fn [{:keys [col-overrides col-names]}]
+   (let [set-function-fn (fn [key selection click-event]
+                           (this-as hot
+                             (let [last-col-num (.. (first selection) -start -col)
+                                   last-col-num-phys (.toPhysicalColumn hot last-col-num)
+                                   col-name (nth col-names last-col-num-phys)
+                                   fn-text (get col-overrides col-name)
+
+                                   modal-child [modal/js-function-entry-modal col-name fn-text]]
+                               (rf/dispatch [:set-modal {:child modal-child}]))))
+
+         clear-function-fn (fn [key selection click-event]
+                             (this-as hot
+                               (let [last-col-num (.. (first selection) -start -col)
+                                     last-col-num-phys (.toPhysicalColumn hot last-col-num)
+                                     col-name (nth col-names last-col-num-phys)]
+                                 (rf/dispatch [:clear-column-function col-name]))))
+
+         disable-fn (fn []
+                     (this-as hot
+                       (let [last-selected (.getSelectedRangeLast hot)
+                             from-col (.. last-selected -from -col)
+                             to-col (.. last-selected -to -col)
+                             from-row (.. last-selected -from -row)]
+
+                         ;; Disable the menu when either more than one column is selected
+                         ;; or when the selection does not start from a cell in the header row.
+                         (or (not= from-col to-col) (not= from-row 0)))))]
+     {:items {"set_function" {:disabled disable-fn
+                              :name "Set js function"
+                              :callback set-function-fn}
+              "clear_function" {:disabled disable-fn
+                                :name "Clear js function"
+                                :callback clear-function-fn}}})))
+
+(rf/reg-sub
+ :cells-style-fn
+ (fn [_ _]
+   {:cell-renderer-fn (rf/subscribe [:cell-renderer-fn])})
+ (fn [{:keys [cell-renderer-fn]}]
+   ;; Returns a function used by the :cells property in Handsontable's options.
+   (fn [row col]
+     (clj->js {:renderer cell-renderer-fn}))))
+
+(rf/reg-sub
+ :cell-renderer-fn
+ (fn [_ _]
+   {:row-likelihoods (rf/subscribe [:row-likelihoods-normed])
+    :missing-cells-flagged (rf/subscribe [:missing-cells-flagged])
+    :conf-thresh (rf/subscribe [:confidence-threshold])
+    :conf-mode (rf/subscribe [:confidence-option [:mode]])
+    :computed-headers (rf/subscribe [:computed-headers])})
+ ;; Returns a cell renderer function used by Handsontable.
+ (fn [{:keys [row-likelihoods missing-cells-flagged conf-thresh conf-mode computed-headers]}]
+   (case conf-mode
+     :none
+     js/Handsontable.renderers.TextRenderer
+
+     :row
+     (fn [& args]
+       ;; These render functions are actually called with this args list:
+       ;; [hot td row col prop value cell-properties]
+       ;; Instead, we are specifying [& args] here to make it cleaner to
+       ;; pass in data to custom rendering functions.
+       (rends/row-wise-likelihood-threshold-renderer args row-likelihoods conf-thresh))
+
+     :cells-existing
+     js/Handsontable.renderers.TextRenderer
+
+     :cells-missing
+     (fn [& args]
+       (rends/missing-cell-wise-likelihood-threshold-renderer args missing-cells-flagged computed-headers)))))
