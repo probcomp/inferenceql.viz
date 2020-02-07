bead4560bda19e9d0f2f8234cd61eeaf78f77942
Harish Tella <htella@mit.edu>
2020-02-07 17:22:27 -0500

    Remove unused subs in main views file.

--- spreadsheets/src/inferenceql/spreadsheets/views.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/views.cljs
@@ -10,9 +10,6 @@
 (defn app
   []
   (let [real-hot-props      @(rf/subscribe [:table/real-hot-props])
-        virtual-hot-props @(rf/subscribe [:table/virtual-hot-props])
-        real-table-in-viz @(rf/subscribe [:viz/real-table-in-viz])
-        virtual-table-in-viz @(rf/subscribe [:viz/virtual-table-in-viz])
         vega-lite-spec @(rf/subscribe [:viz/vega-lite-spec])
         vega-lite-log-level @(rf/subscribe [:viz/vega-lite-log-level])
         generator      @(rf/subscribe [:viz/generator])]
