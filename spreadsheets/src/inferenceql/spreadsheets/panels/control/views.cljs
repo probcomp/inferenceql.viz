44acc2d593f9ad8c75c5d46b89649de88a7dd1fc
Harish Tella <htella@mit.edu>
2020-02-21 15:55:44 -0500

    Remove selection-color selector.

--- spreadsheets/src/inferenceql/spreadsheets/panels/control/views.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/panels/control/views.cljs
@@ -12,18 +12,6 @@
               (rf/dispatch [:control/update-reagent-forms save-fn path value]))
    :doc (fn [] @(rf/subscribe [:control/reagent-forms]))})
 
-(defn selection-color-selector
-  "A reagant component for selecting the table selection color."
-  []
-  (let [template [:div.list-group {:field :single-select :id :selection-color}
-                  [:div.list-group-item {:key :blue} "Blue"]
-                  [:div.list-group-item {:key :green} "Green"]
-                  [:div.list-group-item {:key :red} "Red"]]]
-    [:div#color-selector
-      [:span "Selection color:"]
-      [:br]
-      [forms/bind-fields template reagent-forms-function-map]]))
-
 (defn panel
   "A reagant component. Acts as control and input panel for the app."
   []
@@ -47,5 +35,4 @@
          [:button.toolbar-button.pure-button
           ;; This button performs a no-op currently.
           {:on-click #(do)} "Clear results"]]]
-     [:div.flex-box-space-filler]
-     [selection-color-selector]]))
+     [:div.flex-box-space-filler]]))
