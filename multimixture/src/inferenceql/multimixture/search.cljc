probcomp/inferenceql.viz

commit 5d846e5cd036c1ffbf6bca782db7cd5279cfd1b3

Various Search Implementations (#190)
* Updated imp. samp. search to be parallel + docs, created mmix utils

* Pseudo-det. search + search utils

* Enum. search + docs

* Gibbs row sampling + docs

* Test for Gibbs, enum. search + enum. search function reorder

* Formatting and added explanation to Gibbs tests

* Formatting and added explanation to enumerative tests

multimixture/src/inferenceql/multimixture/search.cljc
@@ -5,6 +5,7 @@
             [metaprob.prelude :as mp]
             [inferenceql.distributions :as idbdist]
             [inferenceql.multimixture :as mmix]
+            [inferenceql.multimixture.utils :as mmix-utils]
             [inferenceql.multimixture.basic-queries :as bq]
             [inferenceql.multimixture.specification :as spec]))
 
@@ -69,8 +70,8 @@
       new-spec)))
 
 (defn importance-resampling
-  [& {:keys [model inputs observation-trace n-particles]
-      :or {inputs [], observation-trace {}, n-particles 1}}]
+  [{:keys [model inputs observation-trace n-particles]
+    :or {inputs [], observation-trace {}, n-particles 1}}]
   (let [particles (mp/replicate n-particles
                                 #(mp/infer-and-score :procedure model
                                                      :inputs inputs
@@ -87,13 +88,13 @@
   "Takes a multimixture specification, views, and a set of rows that have a value
   in the new column that is being added. Returns an updated multimixture
   specification."
-  [spec rows column-key beta-params]
+  [spec rows column-key beta-params {:keys [n-particles] :or {n-particles 100}}]
   (first
    ;; TODO: Setting n-particles to 1 causes IOB errors
    (importance-resampling :model generate-1col-binary-extension
                           :inputs [spec (count rows) column-key beta-params]
                           :observation-trace (mmix/with-rows {} rows)
-                          :n-particles 100)))
+                          {:n-particles n-particles})))
 
 (defn score-rows
   [spec rows new-column-key]
@@ -119,7 +120,7 @@
        (remove (comp nil? val))
        (into {})))
 
-(defn  score-row-probability
+(defn score-row-probability
   [row-generator target-col constraint-cols row]
   (let [target (select-keys row [target-col])
         constraints (constraints-for-scoring-p target-col constraint-cols row)]
@@ -133,8 +134,13 @@
     (map #(score-row-probability row-generator target-col conditional-cols %) data)))
 
 (defn search
-  [spec new-column-key known-rows unknown-rows n-models beta-params]
-  (let [specs (repeatedly n-models #(insert-column spec known-rows new-column-key beta-params))
+  "Conducts classic search via importance sampling, parallelized. If only one spec is given, there
+  will be `n-models` models based on that spec. Otherwise, the number of specs
+  in `spec` must equal n-models."
+  [spec new-column-key known-rows unknown-rows n-models beta-params {:keys [n-particles] :or {n-particles 100}}]
+  (let [specs (if (= (type spec) clojure.lang.PersistentArrayMap)
+                (mmix-utils/prun n-models #(insert-column spec known-rows new-column-key beta-params :n-particles n-particles))
+                (pmap #(insert-column % known-rows new-column-key beta-params {:n-particles n-particles}) spec))
         predictions (mapv #(score-rows % unknown-rows new-column-key)
                           specs)]
     (into []

multimixture/src/inferenceql/multimixture/utils.cljc
@@ -1,0 +1,12 @@
+(ns inferenceql.multimixture.utils)
+
+(defn prun [n f]
+  "Runs `n` parallel calls to function `f`, that is assumed to have
+  no arguments."
+  (apply pcalls (repeat n f)))
+
+(defn transpose
+  "Applies the standard tranpose operation to a collection. Assumes that
+  `coll` is an object capable of having a transpose."
+  [coll]
+  (apply map vector coll))

multimixture/src/inferenceql/multimixture/search/utils.cljc
@@ -1,0 +1,47 @@
+(ns inferenceql.multimixture.search.utils
+  (:require [inferenceql.multimixture :as mmix]
+            [inferenceql.multimixture.utils :as mmix-utils]
+            [metaprob.prelude :as mp]))
+
+(defn normalize-row-probability
+  "Normalizes a collection of non-negative numbers."
+  [coll]
+  (let [z (reduce + coll)]
+    (mapv #(/ % z) coll)))
+
+(defn cluster-row-probability
+  "Determines the probability that a specific cluster component generated the given row."
+  [spec cluster-idx view-idx row]
+   ;; Prior probability * likelihood that cluster generated row, given the row and spec.
+  (let [cluster (get-in spec [:views view-idx cluster-idx])]
+    (* (:probability cluster)
+       (mp/exp (last (mp/infer-and-score :procedure (mmix/cluster-row-generator cluster (get spec :vars))
+                                         :observation-trace (mmix/with-row-values {} row)))))))
+
+(defn view-row-probabilities
+  "Returns a probability table P, where 
+   P[row][cluster-component] = normalized probability (within the specified view)
+                               that `cluster-component` generated `row`."
+  [spec rows]
+  (map-indexed (fn [view-idx view]
+                 (let [view (get-in spec [:views view-idx])]
+                 ;; For each cluster in a view, for each row in cluster, determine the
+                 ;; probability that a cluster generated a row.
+                   (->> view
+                        (map-indexed (fn [cluster-idx _]
+                                       (map #(cluster-row-probability
+                                              spec
+                                              cluster-idx
+                                              view-idx
+                                              %) rows)))
+                      ;; Group probabilities by row, rather than by cluster.
+                        (mmix-utils/transpose)
+                        (map normalize-row-probability))))
+               (get spec :views)))
+
+(defn generate-cluster-row-probability-table
+  "Returns table P where, P[known/unknown][view][row][component] is equivalent
+   to the probability that a known/unknown row within a view by a specific component."
+  [spec known-rows unknown-rows]
+  (map (fn [rows]
+         (view-row-probabilities spec rows)) [known-rows unknown-rows]))

multimixture/src/inferenceql/multimixture/search/deterministic.cljc
@@ -1,59 +1,8 @@
 (ns inferenceql.multimixture.search.deterministic
-  (:require [clojure.spec.alpha :as s]
-            [metaprob.generative-functions :as g :refer [at gen]]
-            [metaprob.prelude :as mp]
-            [inferenceql.multimixture :as mmix]
-            [inferenceql.multimixture.search :as sch]
-            [inferenceql.multimixture.specification :as spec]))
+  (:require [inferenceql.multimixture.utils :as mmix-utils]
+            [inferenceql.multimixture.search.utils :as search-utils]))
 
 #?(:cljs (enable-console-print!))
-
-(defn transpose
-  [coll]
-  (apply map vector coll))
-
-(defn normalize-row-probability
-  "Normalizes a collection of non-negative numbers."
-  [coll]
-  (let [z (reduce + coll)]
-    (mapv #(/ % z) coll)))
-
-(defn cluster-row-probability
-  "Determines the probability that a specific cluster component generated the given row."
-  [spec cluster-idx view-idx row]
-   ;; Prior probability * likelihood that cluster generated row, given the row and spec.
-  (let [cluster (get-in spec [:views view-idx cluster-idx])]
-    (* (:probability cluster)
-       (mp/exp (last (mp/infer-and-score :procedure (mmix/cluster-row-generator cluster (get spec :vars))
-                                         :observation-trace (mmix/with-row-values {} row)))))))
-
-(defn view-row-probabilities
-  "Returns a probability table P, where 
-   P[row][cluster-component] = normalized probability (within the specified view)
-                               that `cluster-component` generated `row`."
-  [spec rows]
-  (map-indexed (fn [view-idx view]
-                 (let [view (get-in spec [:views view-idx])]
-                 ;; For each cluster in a view, for each row in cluster, determine the
-                 ;; probability that a cluster generated a row.
-                   (->> view
-                        (map-indexed (fn [cluster-idx _]
-                                       (map #(cluster-row-probability
-                                              spec
-                                              cluster-idx
-                                              view-idx
-                                              %) rows)))
-                      ;; Group probabilities by row, rather than by cluster.
-                        (transpose)
-                        (map normalize-row-probability))))
-               (get spec :views)))
-
-(defn generate-cluster-row-probability-table
-  "Returns table P where, P[known/unknown][view][row][component] is equivalent
-   to the probability that a known/unknown row within a view by a specific component."
-  [spec known-rows unknown-rows]
-  (map (fn [rows]
-         (view-row-probabilities spec rows)) [known-rows unknown-rows]))
 
 (defn update-beta-params
   "Updates beta params for each component, within each view."
@@ -70,7 +19,7 @@
                                   ;; Ignore indices and group by cluster, not row, to compute
                                   ;; the necessary sum.
                                  (map #(first %))
-                                 (transpose)
+                                 (mmix-utils/transpose)
                                  (map #(apply + %))
                                  (map (fn [param-instance]
                                         {param (+ (param beta-params) param-instance)})))))
@@ -81,7 +30,7 @@
 (defn search
   "Mimicks the behavior of search, but without sampling!"
   [spec new-column-key known-rows unknown-rows beta-params]
-  (let [[known-probs unknown-probs] (generate-cluster-row-probability-table spec known-rows unknown-rows)
+  (let [[known-probs unknown-probs] (search-utils/generate-cluster-row-probability-table spec known-rows unknown-rows)
         num-clusters               (count (get-in spec [:views 0]))
         beta-primes                (map #(update-beta-params
                                           beta-params
@@ -108,35 +57,3 @@
                                  beta-prime)))
                       beta-primes))
                    unknown-rows)))))
-
-;; Simpler example for deterministic search, single view.
-(let [unknown-rows [{"x" 0}
-                    {"x" 5}]
-      known-rows [{"x" 0 "y" true}
-                  {"x" 5 "y" false}]
-      spec {:vars {"x" :gaussian}
-            :views [[{:probability 0.5
-                      :parameters {"x" {:mu 2 :sigma 1}}}
-                     {:probability 0.5
-                      :parameters {"x" {:mu 3 :sigma 1}}}]]}]
-  #_(sch/search spec "y" known-rows unknown-rows 100 {:alpha 0.001 :beta 0.001})
-  #_(search spec "y" known-rows unknown-rows {:alpha 0.001 :beta 0.001}))
-
-;; Simpler example for deterministic search, multiple views.
-(let [unknown-rows [{"x" 0}
-                    {"x" 5}]
-       ; known-rows [{"x" 0 "y" true}
-       ;             {"x" 5 "y" false}]
-      known-rows [{"x" 5 "y" false}]
-      spec {:vars {"x" :gaussian}
-            :views [[{:probability 0.5
-                      :parameters {"x" {:mu 0 :sigma 1}}}
-                     {:probability 0.5
-                      :parameters {"x" {:mu 5 :sigma 1}}}]
-                    [{:probability 0.75
-                      :parameters {"x" {:mu 1 :sigma 1}}}
-                     {:probability 0.25
-                      :parameters {"x" {:mu 3 :sigma 2}}}]]}]
-  #_(search spec "y" known-rows unknown-rows 1000 {:alpha 0.001 :beta 0.001})
-  #_(generate-cluster-row-probability-table spec known-rows unknown-rows)
-  #_(deterministic-search spec "y" known-rows unknown-rows {:alpha 0.001 :beta 0.001}))

multimixture/src/inferenceql/multimixture/search/enumerative.cljc
@@ -1,0 +1,72 @@
+(ns inferenceql.multimixture.search.enumerative
+  (:require [clojure.math.combinatorics :as combo]
+            [inferenceql.multimixture.utils :as mmix-utils]
+            [inferenceql.multimixture.search.utils :as utils]))
+
+(defn rows->all-cluster-configurations
+  "Returns a (lazy) sequence of all possible cluster configurations for rows."
+  [num-rows num-clusters]
+  (if (> num-rows 1)
+    (map flatten
+         (reduce combo/cartesian-product
+                 (repeatedly num-rows
+                             #(range num-clusters))))
+    (combo/cartesian-product (range num-clusters))))
+
+(defn cluster-config->beta-update
+  "Given a cluster configuration and its probability, calculates the
+  weighted beta parameter update."
+  [known-labels config num-clusters prob-config beta-params]
+  (let [freq (frequencies (map vector config known-labels))
+        alpha (:alpha beta-params)
+        beta  (:beta  beta-params)]
+    (mapv (fn [cluster]
+            (let [[alpha' beta'] (map + [alpha beta]
+                                      [(get freq `[~cluster  true] 0)
+                                       (get freq `[~cluster false] 0)])]
+              (* prob-config (/ alpha' (+ alpha' beta')))))
+          (range num-clusters))))
+
+(defn cluster-configs->beta-updates
+  "Given a probability table in the form P[row][cluster] and a list of cluster
+  configurations, returns the list of respective probabilities of said configurations
+  multiplied by the beta-parameter update."
+  [probs known-labels beta-params configs]
+  (let [num-known    (count known-labels)
+        num-clusters (count (first probs))]
+    (map (fn [config]
+           ;; Calculate probability of each assignment and take the product.
+           (let [prob-config (reduce * (map-indexed
+                                        (fn [row assignment]
+                                          (get-in probs [row assignment]))
+                                        config))]
+             ;; Get individual beta parameter update for configuration.
+             (cluster-config->beta-update
+              known-labels
+              config
+              num-clusters
+              prob-config
+              beta-params)))
+         configs)))
+
+(defn search
+  "Mimicks the behavior of search, but without sampling, and it's enumerative!
+  Assumes one view in the specification."
+  [spec new-column-key known-rows unknown-rows beta-params]
+  (let [[known-probs unknown-probs] (utils/generate-cluster-row-probability-table spec known-rows unknown-rows)
+        num-clusters                (count (get-in spec [:views 0]))
+        num-rows                    (+ (count unknown-rows) (count known-rows))
+        known-probs-single-view     (vec (nth   known-probs 0)) ;; hard coded for one view.
+        unknown-probs-single-view   (vec (nth unknown-probs 0)) ;; hard coded for one view.
+        known-labels                (mapv #(get % new-column-key) known-rows)
+        thetas                      (->> (rows->all-cluster-configurations (count known-rows) num-clusters)
+                                         (cluster-configs->beta-updates
+                                          known-probs-single-view
+                                          known-labels
+                                          beta-params)
+                                         (mmix-utils/transpose)
+                                         (map #(apply + %)))]
+    (map-indexed (fn [row _]
+                   ;; Get prediction by multiplying likelihood of cluster by coin weight.
+                   (apply + (map * (get unknown-probs-single-view row) thetas)))
+                 unknown-rows)))

multimixture/src/inferenceql/multimixture/search/gibbs.cljc
@@ -1,0 +1,73 @@
+(ns inferenceql.multimixture.search.gibbs
+  (:require [metaprob.distributions :as dist]
+            [inferenceql.multimixture.search.utils :as utils]))
+
+(defn sample-cluster-assignments
+  "Given a probability table for rows, samples cluster assignments. A valid
+  table is of the form P[view][row][cluster]. Currently assumes one view, so
+  the table should be of the form P[row][cluster]."
+  [probs]
+  (let [n-rows     (count probs)
+        n-clusters (count (first probs))]
+    (mapv #(-> probs
+               (nth %)
+               (dist/categorical))
+          (range n-rows))))
+
+(defn cluster-assignments->thetas
+  "For each cluster c_1, ..., c_k, computes the posterior probability on that cluster,
+  given cluster assignments, and known labels."
+  [assignments beta-params n-clusters labels]
+  ;; `count-map` is of the form {c_i {true n-true false n-false}} for all clusters c_i.
+  (let [count-map (->> (mapv (fn [c r] [c r]) assignments labels)
+                       (frequencies)
+                       (reduce-kv (fn [m k v]
+                                    (let [[cluster label] k]
+                                      (update-in m [cluster label] (fnil #(+ % v) 0))))
+                                  {}))]
+    ;; theta = a' / (a' + b'), a' = # of true labels + a, b' = # of false labels + b
+    (mapv (fn [c]
+            (let [a' (+ (get-in count-map [c true]  0) (:alpha beta-params))
+                  b' (+ (get-in count-map [c false] 0) (:beta  beta-params))]
+              (/ a' (+ a' b'))))
+          (range n-clusters))))
+
+(defn thetas->pred-probs
+  "Given cluster assignments of unknown clusters, as well as cluster posterior probabilities,
+  returns the corresponding posterior probability."
+  [unknown-clusters thetas]
+  (map #(nth thetas %) unknown-clusters))
+
+(defn search
+  "Mimicks the behavior of search, but without sampling, and it's approximated with Gibbs sampling!
+  Assumes one view in the specification."
+  ([spec new-column-key known-rows unknown-rows beta-params]
+    (search spec new-column-key known-rows unknown-rows beta-params 1000))
+  ([spec new-column-key known-rows unknown-rows beta-params iters]
+   (let [[known-probs unknown-probs] (utils/generate-cluster-row-probability-table
+                                      spec
+                                      known-rows
+                                      unknown-rows)
+         unknown-probs-single-view   (nth unknown-probs 0)
+         known-probs-single-view     (nth   known-probs 0)
+         n-clusters                  (count (get-in spec [:views 0]))
+         known-labels                (mapv #(get % new-column-key) known-rows)
+         pred                        (fn [known-clusters unknown-clusters]
+                                       (->> known-labels
+                                            (cluster-assignments->thetas
+                                             known-clusters
+                                             beta-params
+                                             n-clusters)
+                                            (thetas->pred-probs unknown-clusters)))]
+     (loop [unknown-clusters (sample-cluster-assignments unknown-probs-single-view)
+            known-clusters   (sample-cluster-assignments known-probs-single-view)
+            pred-probs       (pred known-clusters unknown-clusters)
+            n-iters          iters]
+       (if (= n-iters 0)
+         (map #(/ % iters) pred-probs)  ;; Return the final result, empirical average.
+         (let [new-unknown-clusters (sample-cluster-assignments unknown-probs-single-view)
+               new-known-clusters   (sample-cluster-assignments   known-probs-single-view)]
+           (recur new-unknown-clusters
+                  new-known-clusters
+                  (map + pred-probs (pred new-known-clusters new-unknown-clusters))
+                  (dec n-iters))))))))

multimixture/test/inferenceql/multimixture/search/deterministic_test.cljc
@@ -24,8 +24,3 @@
                                       unknown-rows
                                       beta-params)]
     (is (= [0.5 0.5] results))))
-
-(deftest normalize-row-probability
-  (let [unnorm-row [0 1 2 3 4]
-        norm-row   [0 (/ 1 10) (/ 1 5) (/ 3 10) (/ 2 5)]]
-    (is (= norm-row (deterministic/normalize-row-probability unnorm-row)))))

multimixture/test/inferenceql/multimixture/search/enumerative_search_test.cljc
@@ -1,0 +1,111 @@
+(ns inferenceql.multimixture.search.enumerative-search-test
+  (:require [clojure.test :as test :refer [deftest testing is]]
+            [inferenceql.multimixture.search.enumerative :as enumerative]))
+
+;; Enumerative tests.
+(deftest rows->all-cluster-configurations
+  "Checks that all configurations are returned for a specified
+  number of rows and clusters."
+  (let [num-rows     2
+        num-clusters 2
+        single-row   1]
+    (is (= `((0 0) (0 1) (1 0) (1 1))
+           (enumerative/rows->all-cluster-configurations
+            num-rows
+            num-clusters)))
+    ;; Number of rows is one.
+    (is (= `((0) (1))
+           (enumerative/rows->all-cluster-configurations
+            single-row
+            num-clusters)))))
+
+(deftest cluster-config->beta-update
+  "Checks that the returned posterior probability is
+  correct regardless of cluster assignment."
+  ;; Walkthrough of the first test.
+  ;; alpha'_0 = alpha_0 + #_true = 0.5 + 1 = 1.5
+  ;; beta'_0  = beta_0  + #_false = 0.5 + 0 = 0.5
+  ;; update_0 = P[diff-config] * alpha'_0 / (alpha'_0 + beta'_0) 
+  ;;          = 0.5 * 1.5 / 2
+  ;;          = 0.375
+  ;; alpha'_1 = alpha_1 + #_true = 0.5 + 0 = 0.5
+  ;; beta'_1  = beta_1  + #_false = 0.5 + 1 = 1.5
+  ;; update_1 = P[diff-config] * alpha'_1 / (alpha'_1 + beta'_1) 
+  ;;          = 0.5 * 0.5 / 2
+  ;;          = 0.125
+  (let [known-labels [true false]
+        diff-config  [0 1]
+        same-config  [0 0]
+        num-clusters 2
+        prob-config  0.5
+        beta-params  {:alpha 0.5 :beta 0.5}]
+    ;; Assignments contains both clusters.
+    (is (= [0.375 0.125]
+           (enumerative/cluster-config->beta-update
+            known-labels
+            diff-config
+            num-clusters
+            prob-config
+            beta-params)))
+    ;; Assignments contain one cluster.
+    (is (= [0.25 0.25]
+           (enumerative/cluster-config->beta-update
+            known-labels
+            same-config
+            num-clusters
+            prob-config
+            beta-params)))))
+
+(deftest cluster-configs->beta-updates
+  "Ensures correct access to probability table and calculation
+  of configuration probability, which weights the posterior
+  probability for a particular cluster."
+  ;; Involved walkthrough, review the walkthrough in 
+  ;; `cluster-config->beta-update` before continuing.
+  ;;
+  ;; P[config_0] = 0.5 * 0.75 = 0.375
+  ;; alpha'_0 = 1.5, beta'_0 = 0.5
+  ;; update_0 =  0.375 * 0.75 = 0.28125
+  ;; alpha'_1 = 0.5, beta'_1 = 1.5
+  ;; update_1 =  0.375 * 0.25 = 0.09375
+  ;;
+  ;; P[config_1] = 0.5 * 0.25 = 0.125
+  ;; alpha'_0 = 1.5, beta'_0 = 1.5
+  ;; update_0 =  0.125 * 0.5 = 0.0625
+  ;; alpha'_1 = 0.5, beta'_1 = 0.5
+  ;; update_1 =  0.125 * 0.5 = 0.0625
+  (let [known-labels [true false]
+        configs      [[0 1] [0 0]]
+        num-clusters 2
+        probs        [[0.5 0.5] [0.25 0.75]]
+        beta-params {:alpha 0.5 :beta 0.5}]
+    (is (= `([0.28125 0.09375] [0.0625 0.0625])
+           (enumerative/cluster-configs->beta-updates
+            probs
+            known-labels
+            beta-params
+            configs)))))
+
+(deftest enumerative-search
+  "Smoke test for enumerative search."
+  ;; `search` sums across all configurations to get predictive probabilities,
+  ;; then sums over c_k for each row, P[c_k | row] * P[True | c_k].
+  (let [spec          {:vars {"x" :gaussian}
+                       :views [[{:probability 0.5
+                                 :parameters {"x" {:mu 1 :sigma 1}}}
+                                {:probability 0.5
+                                 :parameters {"x" {:mu 4 :sigma 1}}}]]}
+        unknown-rows [{"x" 0}
+                      {"x" 5}]
+        known-rows   [{"x" 0 "y" true}
+                      {"x" 5 "y" false}]
+        beta-params   {:alpha 0.5 :beta 0.5}
+        expected     [0.75 0.25]
+        actual       (enumerative/search
+                      spec
+                      "y"
+                      known-rows
+                      unknown-rows
+                      beta-params)]
+    ;; Checks that sum of residuals is less than the threshhold.
+    (is (< (apply  + (map (comp #(max % (- %)) -) expected actual)) 0.01))))

commit 453d75e2d902865138640d5a71785728d668d1eb

Fix bugs in search code related to cljs compilation and destructing.
Fixed bugs related to cljs compilation. There are no pmap and pcalls
functions in cljs. Added reader conditionals to supply alternatives.

Also tests for a collection more idiomatically using (seq coll) instead
of testing the type against clojure.lang.PersistentArrayMap, which is
not accessible by the same exact name in cljs.

Fixed function calls and defs to properly use an options map argument.
e.g. (my-function 1 2 3 {:keyword-1 value-1})
versus (my-function 1 2 3 :keyword-1 value-1)

Fixed calls to search in spreadsheets app that broke after changes
introduced in PR #190.

multimixture/src/inferenceql/multimixture/search.cljc
@@ -70,8 +70,8 @@
       new-spec)))
 
 (defn importance-resampling
-  [{:keys [model inputs observation-trace n-particles]
-    :or {inputs [], observation-trace {}, n-particles 1}}]
+  [model {:keys [inputs observation-trace n-particles]
+          :or {inputs [], observation-trace {}, n-particles 1}}]
   (let [particles (mp/replicate n-particles
                                 #(mp/infer-and-score :procedure model
                                                      :inputs inputs
@@ -91,10 +91,10 @@
   [spec rows column-key beta-params {:keys [n-particles] :or {n-particles 100}}]
   (first
    ;; TODO: Setting n-particles to 1 causes IOB errors
-   (importance-resampling :model generate-1col-binary-extension
-                          :inputs [spec (count rows) column-key beta-params]
-                          :observation-trace (mmix/with-rows {} rows)
-                          {:n-particles n-particles})))
+   (importance-resampling generate-1col-binary-extension
+                          {:inputs [spec (count rows) column-key beta-params]
+                           :observation-trace (mmix/with-rows {} rows)
+                           :n-particles n-particles})))
 
 (defn score-rows
   [spec rows new-column-key]
@@ -138,8 +138,10 @@
   will be `n-models` models based on that spec. Otherwise, the number of specs
   in `spec` must equal n-models."
   [spec new-column-key known-rows unknown-rows n-models beta-params {:keys [n-particles] :or {n-particles 100}}]
-  (let [specs (if (= (type spec) clojure.lang.PersistentArrayMap)
-                (mmix-utils/prun n-models #(insert-column spec known-rows new-column-key beta-params :n-particles n-particles))
+  (let [pmap #?(:clj pmap
+                :cljs map)
+        specs (if (seq spec)
+                (mmix-utils/prun n-models #(insert-column spec known-rows new-column-key beta-params {:n-particles n-particles}))
                 (pmap #(insert-column % known-rows new-column-key beta-params {:n-particles n-particles}) spec))
         predictions (mapv #(score-rows % unknown-rows new-column-key)
                           specs)]

multimixture/src/inferenceql/multimixture/utils.cljc
@@ -3,7 +3,8 @@
 (defn prun [n f]
   "Runs `n` parallel calls to function `f`, that is assumed to have
   no arguments."
-  (apply pcalls (repeat n f)))
+  #?(:clj (apply pcalls (repeat n f))
+     :cljs (repeatedly n f)))
 
 (defn transpose
   "Applies the standard tranpose operation to a collection. Assumes that

spreadsheets/src/inferenceql/spreadsheets/components/query/events.cljs
@@ -65,7 +65,7 @@
    (let [rows (table-db/dataset-rows db)
          headers (table-db/dataset-headers db)
          search-row (merge example-row {search-column true})
-         result (search/search model/spec search-column [search-row] rows n-models beta-params)]
+         result (search/search model/spec search-column [search-row] rows n-models beta-params {})]
      {:dispatch [:table/set rows headers {:scores result}]})))
 
 (rf/reg-event-fx
@@ -125,7 +125,7 @@
 
          example-rows (create-search-examples pos-rows neg-rows)
 
-         scores (search/search model/spec search-column example-rows unlabeled-rows n-models beta-params)
+         scores (search/search model/spec search-column example-rows unlabeled-rows n-models beta-params {})
          scores-ids-map (zipmap unlabeled-ids scores)
 
          scores-ids-map-lab (create-scores-map-for-labeled-rows pos-ids neg-ids)