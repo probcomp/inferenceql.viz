98bad7713fb625107a8500c8c0495a3ed310e08b
Harish Tella <htella@mit.edu>
2020-04-10 15:24:39 -0500

    Use the same set of colors for every cluster.

--- spreadsheets/src/inferenceql/spreadsheets/clojure_conj/animation/plot.clj
+++ spreadsheets/src/inferenceql/spreadsheets/clojure_conj/animation/plot.clj
@@ -159,8 +159,9 @@
                 "#98df8a" "#d62728" "#ff9896" "#9467bd" "#c5b0d5"
                 "#8c564b" "#c49c94" "#e377c2" "#f7b6d2" "#7f7f7f"
                 "#c7c7c7" "#bcbd22" "#dbdb8d" "#17becf" "#9edae5"]
-        color-list (cycle (partition 2 colors))
-
+        ;; color-list (cycle (partition 2 colors))
+        ;; NOTE: only using a single color.
+        color-list (repeat (first (partition 2 colors)))
 
         grab-colors (fn [vid cids-in-view]
                       (let [view-key-str (str "view-" vid)
