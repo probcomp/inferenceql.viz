16bc5f5ea41dfa686fcbf2c14e03c51b3e794d6b
Harish Tella <htella@mit.edu>
2020-07-07 15:59:14 -0400

    Add new interceptor for just logging event name.

--- spreadsheets/src/inferenceql/spreadsheets/events/interceptors.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/events/interceptors.cljs
@@ -2,6 +2,8 @@
   (:require [clojure.spec.alpha :as s]
             [expound.alpha :as expound]
             [re-frame.core :as rf]
+            [re-frame.interceptor :refer [->interceptor get-coeffect]]
+            [re-frame.loggers :refer [console]]
             [inferenceql.spreadsheets.db :as db]))
 
 (defn check-and-throw
@@ -12,7 +14,22 @@
     (throw (ex-info (str "db does not satisfy spec: " (s/explain-str a-spec db))
                     (s/explain-data a-spec db)))))
 
-(def check-spec (rf/after #(check-and-throw % ::db/db)))
+(def check-spec
+  "An interceptor which validates the entire db against its spec."
+  (rf/after #(check-and-throw % ::db/db)))
+
+(def log-name
+  "An interceptor which logs an event handler's name to the console.
+  Output includes the event vector. "
+  (->interceptor
+    :id :log-name
+    :before (fn debug-before
+              [context]
+              (console :log "Handling re-frame event:" (get-coeffect context :event))
+              context)
+    :after  (fn debug-after
+              [context]
+              context)))
 
 (def event-interceptors
   "A default set of event interceptors to use within events across the app."
