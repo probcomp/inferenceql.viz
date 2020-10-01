9a61c60fb06a9dd2b4e4e114f3a9e68f77b7a594
Harish Tella <htella@mit.edu>
2020-10-01 14:16:20 -0400

    WIP: adding docstrings

--- spreadsheets/src/inferenceql/spreadsheets/panels/upload/effects.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/panels/upload/effects.cljs
@@ -9,7 +9,20 @@
             [clojure.edn :as edn]))
 
 (defn handle-reads
-  "TODO: Write me"
+  "Reads files off of a channel and places them in a config map.
+
+  This reads `num-reads` times off of the `file-reads` channel. It takes each read and places it in
+  the appropriate section of a config map. When all the reads have been processed, the
+  `on-success` event vector is dispatched with the new config map.
+
+  Args:
+    `file-reads` -- (core.async channel)
+    `num-reads` -- (int) number of files to read off of `file-reads`.
+    `config` -- (map) a config map that will have file data inserted into it. This map comes from
+      a config file specified as part of a demo, or a mock one is submitted for a basic dataset
+      and model.
+    `on-success` -- (vector) a reframe event vector for dispatching on success.
+    `on-failure` -- (vector) a reframe event vector for dispatching on failure."
   [file-reads num-reads config on-success on-failure]
   (let [file-reads-batched (async/into [] (async/take num-reads file-reads))]
     (go
@@ -29,10 +42,28 @@
            (rf/dispatch (conj on-failure failure-messages))))))))
 
 (defn ^:effect read-files-effect
-  "TODO: Write me"
+  "Reads files selected as part of form for adding a new dataset and model.
+
+  This reads the specified files and places them in a mock config map which will passed to the
+  :on-success event specified.
+
+  Args:
+    `params` -- A map of parameters with the following keys.
+      :dataset-name -- (string) A name for the new dataset.
+      :model-name -- (string) A name for the new model.
+      :dataset-file -- (File) File object specifiying a dataset.
+      :dataset-schema-file -- (File) File object specifiying a schema for the dataset.
+      :model-file -- (File) File object specifiying a model.
+      :on-success -- (vector) A reframe event vector to dispatch on success.
+      :on-failure -- (vector) A reframe event vector to dispatch on failure.
+
+  Dispatched by:
+    The re-frame event :upload/read-files, which was dispatched when the form was originally
+      submitted."
   [params]
-  (let [{:keys [files dataset-name model-name on-success on-failure]} params
-        {:keys [dataset-file dataset-schema-file model-file]} files
+  (let [{:keys [dataset-name model-name
+                dataset-file dataset-schema-file model-file
+                on-success on-failure]} params
         required-files [dataset-file dataset-schema-file model-file]]
     (if-not (every? #(instance? js/File %) required-files)
       (rf/dispatch (conj on-failure "Not all required files were specified."))
@@ -67,17 +98,34 @@
             (.readAsText rdr file-obj)))
 
         (handle-reads file-reads (count files) mock-config on-success on-failure)))))
-(rf/reg-fx :upload/read-files-effect read-files-effect)
+(rf/reg-fx :upload/read-files-effect
+           read-files-effect)
 
 (defn end-with-slash
-  "TODO: This is needed so that"
+  "Adds a final forward slash at the end of `url` if it is not already present."
   [url]
   (if (not= "/" (last (seq url))) ;; Last character is not a slash
     (str url "/")
     url))
 
 (defn ^:effect read-url-effect
-  "TODO: write me"
+  "Uses a magic-url to make https requests for obtaining many files for loading a demo.
+
+  TODO -----
+
+  This reads the specified files and places them in a mock config map which will passed to the
+  :on-success event specified.
+
+  Args:
+    `params` -- A map of parameters with the following keys.
+      :url -- (string)
+      :use-creds -- (bool)
+      :on-success -- (vector) A reframe event vector to dispatch on success.
+      :on-failure -- (vector) A reframe event vector to dispatch on failure.
+
+  Dispatched by:
+    The re-frame event :upload/read-url, which was dispatched when the form was originally
+      submitted."
   [params]
   (let [{:keys [url use-creds on-success on-failure]} params
         cors-proxy-url "https://whispering-taiga-62040.herokuapp.com/"
@@ -133,4 +181,5 @@
                  :response-format (ajax.core/text-response-format)})))
 
            (handle-reads file-reads (count data-to-fetch) config on-success on-failure)))))))
-(rf/reg-fx :upload/read-url-effect read-url-effect)
+(rf/reg-fx :upload/read-url-effect
+           read-url-effect)
--- spreadsheets/src/inferenceql/spreadsheets/panels/upload/events.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/panels/upload/events.cljs
@@ -16,79 +16,133 @@
  (fn [db [_ new-val]]
    (assoc-in db [:upload-panel :display] new-val)))
 
-(rf/reg-event-fx
- :upload/read-files
- event-interceptors
- (fn [{:keys [_db]} [_ form-data]]
-   (let [{:keys [dataset-name model-name]} form-data
-         file-objs (select-keys form-data [:dataset-file :dataset-schema-file :model-file])]
-     {:upload/read-files-effect {:files file-objs
-                                 :dataset-name dataset-name
-                                 :model-name model-name
-                                 :on-success [:upload/process-config]
-                                 :on-failure [:upload/read-failed]}
-      :dispatch-n [[:upload/set-display false]
-                   [:table/clear]]})))
+(defn ^:event-fx read-files
+  "Starts the processing of a form for selecting a new dataset and model via local files.
 
-(rf/reg-event-fx
- :upload/read-url
- event-interceptors
- (fn [{:keys [_db]} [_ form-data]]
-   (let [{:keys [url use-creds]} form-data]
-     {:upload/read-url-effect {:url url
-                               :use-creds use-creds
-                               :on-success [:upload/process-config]
-                               :on-failure [:upload/read-failed]}
-      :dispatch-n [[:upload/set-display false]
-                   [:table/clear]]})))
+  Args:
+    `form-data` - A map of data entered in the form. This includes File objects that were selected
+      via browser file selectors.
 
-(rf/reg-event-fx
- :upload/process-config
- event-interceptors
- (fn [{:keys [_db]} [_ config]]
-   (let [datasets (medley/map-vals (fn [dataset]
-                                     (let [schema (edn/read-string (:schema dataset))
-                                           csv-data (csv/parse (:data dataset))
-                                           rows (csv-utils/csv-data->clean-maps
-                                                 schema csv-data {:keywordize-cols true})]
-                                       (merge dataset {:schema schema
-                                                       :csv-data csv-data
-                                                       :rows rows})))
-                                   (:datasets config))
+  Triggered when:
+    The user submits the form.
 
-         models (medley/map-vals (fn [model]
-                                   (let [model-extension (last (str/split (:filename model) #"\."))
-                                         csv-data (get-in datasets [(:dataset model) :rows])
-                                         model-obj (case model-extension
-                                                     "edn"
-                                                     (gpm/Multimixture (edn/read-string (:data model)))
-                                                     "json"
-                                                     (first
-                                                      (bayesdb-import/xcat-gpms
-                                                        (js->clj (.parse js/JSON (:data model)))
-                                                        csv-data)))]
-                                     (assoc model :model-obj model-obj)))
-                                 (:models config))
+  Effects returned:
+    :upload/read-files-effect -- Continues the processing of the submitted files.
+    :dispatch [:upload/set-display] -- Clears the display of the form.
+    :dispatch [:table/clear] -- Clears all data displayed in the table."
+  [{:keys [_db]} [_ form-data]]
+  (let [{:keys [dataset-name model-name
+                dataset-file dataset-schema-file model-file]} form-data]
+    {:upload/read-files-effect {:dataset-name dataset-name
+                                :model-name model-name
+                                :dataset-file dataset-file
+                                :dataset-schema-file dataset-schema-file
+                                :model-file model-file
+                                :on-success [:upload/process-config]
+                                :on-failure [:upload/read-failed]}
+     :dispatch-n [[:upload/set-display false]
+                  [:table/clear]]}))
+(rf/reg-event-fx :upload/read-files
+                 event-interceptors
+                 read-files)
 
-         geodata (medley/map-vals (fn [geodatum] (update geodatum :data #(.parse js/JSON %)))
-                                  (:geodata config))
+(defn ^:event-fx read-url
+  "Starts the processing of a form for submitting a magic-url which will load a new demo
 
-         ;; Filtering out the relevent keys before storing.
-         datasets-to-store (medley/map-vals
-                            #(select-keys % [:rows :schema :default-model
-                                             :geodata-name :geo-id-col])
-                            datasets)
-         models-to-store (medley/map-vals :model-obj models)]
-     ;; TODO: catch conversion errors.
-     (if true
-       {:dispatch-n [[:store/datasets datasets-to-store]
-                     [:store/models models-to-store]
-                     (when geodata
-                       [:store/geodata geodata])]}
-       {:dispatch [:upload/read-failed "TODO: write error message for conversion."]}))))
+  A loading a demo means loading all datasets, models, and geodata related to a particular demo.
 
-(rf/reg-event-fx
-  :upload/read-failed
-  event-interceptors
-  (fn [{:keys [_db]} [_ error]]
-    {:js/console-error error}))
+  Args:
+    `form-data` - A map of data entered in the form.
+
+  Triggered when:
+    The user submits the form.
+
+  Effects returned:
+    :upload/read-url-effect -- Continues the processing of the submitted url.
+    :dispatch [:upload/set-display] -- Clears the display of the form.
+    :dispatch [:table/clear] -- Clears all data displayed in the table."
+  [{:keys [_db]} [_ form-data]]
+  (let [{:keys [url use-creds]} form-data]
+    {:upload/read-url-effect {:url url
+                              :use-creds use-creds
+                              :on-success [:upload/process-config]
+                              :on-failure [:upload/read-failed]}
+     :dispatch-n [[:upload/set-display false]
+                  [:table/clear]]}))
+(rf/reg-event-fx :upload/read-url
+                 event-interceptors
+                 read-url)
+
+(defn ^:event-fx process-config
+  "Processes a config map that contains datasets, models, and geodata and stores them.
+
+  The sections in a config map contain fairly raw data. They need to be transformed before saving
+  them in the app-db.
+
+  Args:
+    `config` - A map containing sections of new datasets, models, and geodata.
+    {:datasets {:dataset-name {...}}
+     :models {:model-name {...}}
+     :geodata {:geodata-name {...}}}
+
+  Triggered when:
+    Effects which process files or urls for new data and models succeed.
+
+  Effects returned:
+    :dispatch [:store/datasets] -- Stores datasets read from config.
+    :dispatch [:store/models] -- Stores models read from config.
+    :dispatch [:store/geodata] -- Stores geodata read from config.
+    :dispatch [:upload/read-failed] -- Outputs an error message about the failure."
+  [{:keys [_db]} [_ config]]
+  (let [datasets (medley/map-vals (fn [dataset]
+                                    (let [schema (edn/read-string (:schema dataset))
+                                          csv-data (csv/parse (:data dataset))
+                                          rows (csv-utils/csv-data->clean-maps
+                                                schema csv-data {:keywordize-cols true})]
+                                      (merge dataset {:schema schema
+                                                      :csv-data csv-data
+                                                      :rows rows})))
+                                  (:datasets config))
+
+        models (medley/map-vals (fn [model]
+                                  (let [model-extension (last (str/split (:filename model) #"\."))
+                                        csv-data (get-in datasets [(:dataset model) :rows])
+                                        model-obj (case model-extension
+                                                    "edn"
+                                                    (gpm/Multimixture (edn/read-string (:data model)))
+                                                    "json"
+                                                    (first
+                                                     (bayesdb-import/xcat-gpms
+                                                      (js->clj (.parse js/JSON (:data model)))
+                                                      csv-data)))]
+                                    (assoc model :model-obj model-obj)))
+                                (:models config))
+
+        geodata (medley/map-vals (fn [geodatum] (update geodatum :data #(.parse js/JSON %)))
+                                 (:geodata config))
+
+        ;; Selecting the necessary keys before storing.
+        datasets-to-store (medley/map-vals
+                           #(select-keys % [:rows :schema :default-model
+                                            :geodata-name :geo-id-col])
+                           datasets)
+        ;; Selecting just the actual gpm.
+        models-to-store (medley/map-vals :model-obj models)]
+    ;; TODO: catch conversion errors.
+    (if true
+      {:dispatch-n [[:store/datasets datasets-to-store]
+                    [:store/models models-to-store]
+                    (when geodata
+                      [:store/geodata geodata])]}
+      {:dispatch [:upload/read-failed "TODO: write error message for conversion."]})))
+(rf/reg-event-fx :upload/process-config
+                 event-interceptors
+                 process-config)
+
+(defn ^:event-fx read-failed
+  ""
+  [{:keys [_db]} [_ error]]
+  {:js/console-error error})
+(rf/reg-event-fx :upload/read-failed
+                 event-interceptors
+                 read-failed)
