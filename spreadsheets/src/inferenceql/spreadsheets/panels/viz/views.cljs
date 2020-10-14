63f62f6faac67d92ddc1313b8fc857fcdbeb8dad
Harish Tella <htella@mit.edu>
2020-10-14 12:28:48 -0400

    Remove unneeded when block

--- spreadsheets/src/inferenceql/spreadsheets/panels/viz/views.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/panels/viz/views.cljs
@@ -103,8 +103,7 @@
 
       :reagent-render
       (fn [spec opt generators pts-store]
-        (when spec
-          [:div#viz-container
-           [:div.flex-box-space-filler-20]
-           [:div {:ref #(swap! dom-nodes assoc :vega-node %)}]
-           [:div.flex-box-space-filler-20]]))})))
+        [:div#viz-container
+         [:div.flex-box-space-filler-20]
+         [:div {:ref #(swap! dom-nodes assoc :vega-node %)}]
+         [:div.flex-box-space-filler-20]])})))
