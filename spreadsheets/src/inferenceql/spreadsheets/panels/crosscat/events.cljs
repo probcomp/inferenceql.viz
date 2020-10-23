128f1afb6b7d480afcda2d6f7aa064b2ae00b81e
Harish Tella <htella@mit.edu>
2020-10-23 16:45:39 -0500

    Add a button to set option to :gamma

--- spreadsheets/src/inferenceql/spreadsheets/panels/crosscat/events.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/panels/crosscat/events.cljs
@@ -9,7 +9,8 @@
     `new-val` - (keyword) The new value to set for option.
 
   Triggered when:
-    The user selects a different option in the Crosscat viz dropdown menu for option."
+    The user selects a different option in the dropdown menu for option.
+    The user presses the set option to gamma buttton."
   [db [_ new-val]]
   (assoc-in db [:crosscat-panel :option] new-val))
 (rf/reg-event-db :crosscat/set-option
--- spreadsheets/src/inferenceql/spreadsheets/panels/crosscat/views.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/panels/crosscat/views.cljs
@@ -1,6 +1,6 @@
 (ns inferenceql.spreadsheets.panels.crosscat.views
   (:require [re-frame.core :as rf]
-            [re-com.core :refer [box h-box v-box gap single-dropdown]]))
+            [re-com.core :refer [box h-box v-box gap single-dropdown button]]))
 
 (defn viz
   "A reagant component for displaying a Crossscat visualization."
@@ -10,11 +10,14 @@
      :gap "10px"
      :margin "10px 10px 10px 10px"
      :children [[:h4 "Crosscat Viz"]
-                [:span (str "Current value for option is " option)]
+                [:h5 (str "Current value for option is " option)]
                 [single-dropdown
                  :choices   [{:id :alpha :label "Alpha"}
                              {:id :beta :label "Beta"}
                              {:id :gamma :label "Gamma"}]
                  :model     option
-                 :width     "300px"
-                 :on-change #(rf/dispatch [:crosscat/set-option %])]]]))
+                 :width     "100px"
+                 :on-change #(rf/dispatch [:crosscat/set-option %])]
+                [button
+                 :label "Set option to :gamma"
+                 :on-click #(rf/dispatch [:crosscat/set-option :gamma])]]]))
