b96ee16afaf9cf11401a1037911185b79e1fb16c
Harish Tella <htella@mit.edu>
2020-10-23 16:40:05 -0500

    Add a dropdown for changing the value of option.

--- spreadsheets/src/inferenceql/spreadsheets/panels/crosscat/views.cljs
+++ spreadsheets/src/inferenceql/spreadsheets/panels/crosscat/views.cljs
@@ -1,11 +1,20 @@
 (ns inferenceql.spreadsheets.panels.crosscat.views
   (:require [re-frame.core :as rf]
-            [re-com.core :refer [box v-box]]))
+            [re-com.core :refer [box h-box v-box gap single-dropdown]]))
 
 (defn viz
   "A reagant component for displaying a Crossscat visualization."
   []
   (let [option @(rf/subscribe [:crosscat/option])]
-    [box :child [:div
-                 [:h4 "Crosscat Viz"]
-                 [:span (str "Current value for option is " option)]]]))
+    [v-box
+     :gap "10px"
+     :margin "10px 10px 10px 10px"
+     :children [[:h4 "Crosscat Viz"]
+                [:span (str "Current value for option is " option)]
+                [single-dropdown
+                 :choices   [{:id :alpha :label "Alpha"}
+                             {:id :beta :label "Beta"}
+                             {:id :gamma :label "Gamma"}]
+                 :model     option
+                 :width     "300px"
+                 :on-change #(rf/dispatch [:crosscat/set-option %])]]]))
