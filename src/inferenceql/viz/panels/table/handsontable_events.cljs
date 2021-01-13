f09819865cefd1f9341b8046ccde20b2520aa7b3
Harish Tella <htella@mit.edu>
2021-01-13 11:59:25 -0500

    WIP

--- src/inferenceql/viz/panels/table/handsontable_events.cljs
+++ src/inferenceql/viz/panels/table/handsontable_events.cljs
@@ -4,7 +4,8 @@
             [inferenceql.viz.panels.table.selections :as selections]
             [inferenceql.viz.panels.table.db :as table-db]
             [inferenceql.viz.events.interceptors :refer [event-interceptors]]
-            [inferenceql.viz.panels.control.db :as control-db]))
+            [inferenceql.viz.panels.control.db :as control-db]
+            [medley.core :as medley]))
 
 (rf/reg-event-db
  :hot/after-selection-end
@@ -78,6 +79,28 @@
   (let [valid-change-sources #{"edit" "CopyPaste.paste" "Autofill.fill"}]
     (some? (valid-change-sources source))))
 
+(defn merge-row-updates
+  "Merges `updates` into `rows`.
+  Both `updates` and `rows` are a maps where keys are row-ids and vals are rows
+  (or row updates) in the case of `updates`."
+  [rows updates]
+  (let [merge-op (fnil (partial merge-with merge) {} {})
+        merged-rows (merge-op rows updates)
+
+        ;; Updates will sometimes have nil or "" as the new value for a particular attribute
+        ;; in a row. This means the user has entered "" in the cell or has deleted the cell's value.
+        ;; For these cases we want to remove these values and their corresponding keys
+        ;; from the map representing the row.
+        empty-cell? #(or (nil? %) (= "" %))]
+
+    (reduce (fn [acc [row-id row]]
+              (let [clean-row (medley/remove-vals empty-cell? row)]
+                (if (seq clean-row)
+                  (assoc acc row-id clean-row)
+                  acc)))
+            {}
+            merged-rows)))
+
 (rf/reg-event-db
   :hot/before-change
   event-interceptors
@@ -91,7 +114,6 @@
                               (assert (= col :label))
                               (assoc-in acc [row-id col] new-val)))
                           {}
-                          changes)
-          merge-op (fnil (partial merge-with merge) {} {})]
+                          changes)]
       ;; Stage the changes in the db. The Handsontable itself already has the updates.
-      (update-in db [:table-panel :changes :existing] merge-op updates))))
+      (update-in db [:table-panel :changes :existing] merge-row-updates updates))))
