e26e23a47e24cc93a92e6e8081c0280f3c58c3d5
Harish Tella <htella@mit.edu>
2021-01-21 14:32:16 -0500

    wip

--- src/inferenceql/viz/panels/table/png_export.cljs
+++ src/inferenceql/viz/panels/table/png_export.cljs
@@ -7,19 +7,22 @@
 (defn render-table-pngs []
   (rf/dispatch-sync [:table/set
                      [{:foo 3 :bar 3} {:foo 4 :bar 4}]
-                     [:foo :bar]]))
-  
+                     [:foo :bar]])
+  ;;(rf/dispatch-sync [:table/export-png])
+  (let [table (.querySelector js/document "#table-container")]
+    (.log js/console :foo table)
+    (doto (js/html2canvas table)
+      (.then (fn [canvas]
+               (let [save-fn (fn [blob]
+                               (js/saveAs blob "image.png"))]
+                 (.toBlob canvas save-fn))))))
 
-
-(rf/reg-event-fx
- :table/render-png
- event-interceptors
- (fn [_ [_]]
-   #_(let [node (.getElementById js/document "table-container")]
+ #_(fn [_ [_]]
+     (let [node (.getElementById js/document "table-container")]
        (doto (.toPng js/domtoimage node)
          (.then (fn [data-url]
                   (let [img (js/Image.)]
                     (set! (.-src img) data-url
                            (.appendChild (.-body js/document) img)))))
          (.catch (fn [res]))))
-   {}))
+     {}))
