9ff15cfd33d3be34bc1801a18de22fbd45ad859f
Harish Tella <htella@mit.edu>
2021-01-22 14:32:24 -0500

    get basic download script working

--- download-tables-pngs.js
+++ download-tables-pngs.js
@@ -1,17 +1,22 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.setViewport({
-    width: 1024,
-    height: 900,
+    width: 1680,
+    height: 1000,
     // should I use this scale factor
     deviceScaleFactor: 2,
   });
+
+  await page._client.send('Page.setDownloadBehavior', {
+    behavior: 'allow',
+    downloadPath: 'table-pngs/'
+  })
   // what is networkidle2
   await page.goto('http://localhost:9500', {waitUntil: 'networkidle2'});
-  await page.screenshot({path: 'example.png'});
+  //await page.screenshot({path: 'example.png'});
 
   await browser.close();
 })();
