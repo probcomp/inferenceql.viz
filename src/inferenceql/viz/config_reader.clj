bbb9604e5e45cf3e2f0b4c9327aa9d0ef9507b17
Harish Tella <htella@mit.edu>
2021-04-02 14:14:03 -0400

    WIP: breaking things

--- src/inferenceql/viz/config_reader.clj
+++ src/inferenceql/viz/config_reader.clj
@@ -5,7 +5,13 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [aero.core :as aero]
-            [inferenceql.inference.gpm :as gpm]))
+            [inferenceql.inference.gpm :as gpm]
+            [inferenceql.inference.gpm.column :as column]
+            [inferenceql.inference.gpm.crosscat :as xcat]
+            [inferenceql.inference.gpm.view :as view]
+            [inferenceql.inference.gpm.primitive-gpms.bernoulli :as bernoulli]
+            [inferenceql.inference.gpm.primitive-gpms.categorical :as categorical]
+            [inferenceql.inference.gpm.primitive-gpms.gaussian :as gaussian]))
 
 (defmethod aero/reader 'txt
   [_ _ s]
@@ -15,9 +21,17 @@
   [_ _ s]
   (-> s (io/resource) (slurp) (csv/read-csv) (vec)))
 
+(def readers
+  {'inferenceql.inference.gpm.crosscat.XCat xcat/map->XCat
+   'inferenceql.inference.gpm.view.View view/map->View
+   'inferenceql.inference.gpm.column.Column column/map->Column
+   'inferenceql.inference.gpm.primitive_gpms.bernoulli.Bernoulli bernoulli/map->Bernoulli
+   'inferenceql.inference.gpm.primitive_gpms.categorical.Categorical categorical/map->Categorical
+   'inferenceql.inference.gpm.primitive_gpms.gaussian.Gaussian #_vector gaussian/map->Gaussian})
+
 (defmethod aero/reader 'edn
   [_ _ s]
-  (->> s (io/resource) (slurp) (edn/read-string {:readers gpm/readers})))
+  (->> s (io/resource) (slurp) (edn/read-string {:readers readers})))
 
 (defmethod aero/reader 'json
   [_ _ s]
