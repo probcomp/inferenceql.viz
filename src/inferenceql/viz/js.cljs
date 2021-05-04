61e52b7ff14402c9ddea10c0f4d36cd45ed35d81
Harish Tella <htella@mit.edu>
2021-05-04 17:32:16 -0400

    Changes to read and coerce data

--- src/inferenceql/viz/js.cljs
+++ src/inferenceql/viz/js.cljs
@@ -4,29 +4,30 @@
             [goog.dom :as dom]
             [reagent.dom :as rdom]
             [inferenceql.query.data :refer [row-coercer]]
-            [inferenceql.viz.csv]
+            [inferenceql.viz.csv :as csv]
             [inferenceql.viz.panels.table.handsontable :refer [default-hot-settings]]
             [inferenceql.viz.panels.table.views :refer [handsontable]]
             [inferenceql.viz.panels.table.subs :refer [column-settings]]
             [inferenceql.viz.panels.viz.views :as viz-views]
             [inferenceql.viz.panels.viz.vega :as vega]
-            ;; Functions for running iql queries from JS.
-            [inferenceql.query.js]))
+            [inferenceql.query.js] ; Used to run queries from JS. Not used directly.
+            [medley.core :as medley]))
+
+(defn clj-schema
+  [js-schema]
+  (medley/map-kv (fn [k v]
+                   [(keyword k) (keyword v)])
+                 (js->clj js-schema)))
 
 (defn ^:export read_schema
   [schema-string]
   (clj->js (edn/read-string schema-string)))
 
-#_(defn ^:export read_and_coerce_csv
-    [csv-text schema]
-    (let [csv-vecs (-> csv-text goog.csv/parse js->clj)]
-      (csv-data->clean-maps schema csv-vecs {:keywordize-cols true})))
-
-(defn ^:export simple_test
-  []
-  (let [node (dom/createElement "div")]
-    (rdom/render [:h1 "hello!"] node)
-    node))
+(defn ^:export read_and_coerce_csv
+  [csv-text schema]
+  (let [csv-vecs (-> csv-text goog.csv/parse js->clj)
+        schema (clj-schema schema)]
+    (clj->js (csv/clean-csv-maps schema csv-vecs))))
 
 (defn ^:export table
   ([data]
@@ -37,7 +38,6 @@
 
          ;; Potentially grabbing the columns from the keys in the first row of data.
          cols (or cols (->> data first js-keys))
-
          height (cond
                   (false? v-scroll) "auto"
                   (some? height) height
@@ -46,18 +46,15 @@
                   (let [data-height (+ (* (count data) 22) 38)]
                     (min data-height 500)))
 
-         node (dom/createElement "div")
          settings (-> default-hot-settings
                       (assoc-in [:settings :data] data)
                       (assoc-in [:settings :colHeaders] cols)
                       (assoc-in [:settings :columns] (column-settings cols))
                       (assoc-in [:settings :height] height)
-                      (assoc-in [:settings :width] "100%"))]
-     ;; todo: move component up
-     (rdom/render [handsontable
-                   {:style {:padding-bottom "5px"}}
-                   settings]
-                  node)
+                      (assoc-in [:settings :width] "100%"))
+         node (dom/createElement "div")
+         hot-component [handsontable {:style {:padding-bottom "5px"}} settings]]
+     (rdom/render hot-component node)
      node)))
 
 (defn ^:export plot
@@ -66,15 +63,11 @@
                      (for [col cols]
                        (keyword col)))
 
-        schema (js->clj schema :keywordize-keys true)
+        schema (clj-schema schema)
         data (js->clj data :keywordize-keys true)
-
-        coercer (row-coercer schema)
-        data (mapv coercer data)
-
         spec (vega/generate-spec schema data selections)
-        comp [viz-views/vega-lite spec {:actions false} nil nil]
 
-        n (dom/createElement "div")]
-    (rdom/render comp n)
-    n))
+        comp [viz-views/vega-lite spec {:actions false} nil nil]
+        node (dom/createElement "div")]
+    (rdom/render comp node)
+    node))
