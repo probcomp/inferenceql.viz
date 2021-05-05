97fcf220632ed479a26cf3fb6c208b29e4e59251
Harish Tella <htella@mit.edu>
2021-05-05 17:13:36 -0400

    Deal with ensembles

--- spreadsheets/src/inferenceql/program_animation/core.clj
+++ spreadsheets/src/inferenceql/program_animation/core.clj
@@ -32,13 +32,17 @@
 
 (def schema (get-schema bayesdb-dump))
 ;; TODO: schema will not have all columns here. Use schema from file?
+
 (def rows (csv-utils/csv-data->clean-maps schema csv {:keywordize-cols true}))
 
-(def program (->> (bayesdb-import/xcat-gpms bayesdb-dump rows)
-                  (first) ;; Only one model here.
-                  (crosscat/xcat->mmix)
-                  (multimix/template-data)
-                  (render (:js-model-template config))))
+(def xcat-models (bayesdb-import/xcat-gpms bayesdb-dump rows))
+
+(def programs
+  (for [model xcat-models]
+    (->> model
+         (crosscat/xcat->mmix)
+         (multimix/template-data)
+         (render (:js-model-template config)))))
 
 (defn clean-output-dirs
   "Make clean output directories."
@@ -55,26 +59,41 @@
 (defn -main []
   (clean-output-dirs)
 
-  (println "Writing model")
-  (let [i 0
-        svg-file (str "images-svg/model-" i ".svg")
-        jpg-file (str "images-jpeg/model-" i ".jpg")]
-    (spit svg-file (clygments/highlight program :js :svg {:style "xcode"}))
-    (sh "svgexport" svg-file jpg-file "pad" "2x" "100%"))
+  (doseq [[i program] (map-indexed vector programs)]
+    (println (str "Writing model " i))
+    (let [svg-file (str "images-svg/model-" i ".svg")
+          jpg-file (str "images-jpeg/model-" i ".jpg")]
+      (spit svg-file (clygments/highlight program :js :svg {:style "xcode"}))
+      (sh "svgexport" svg-file jpg-file "pad" "2x" "100%")))
 
   ;; Resize all images to the max width and height between all of them.
-  #_(let [file-name-wildcard "images-jpeg/model-*.jpg"
-          file-sizes (:out (sh "identify" "-format" "%w %h\n" file-name-wildcard))
-          max-size (:out (sh "awk" "($1>w){w=$1} ($2>h){h=$2} END{print w\"x\"h}" :in file-sizes))
-          size-string (str (str/trim-newline max-size)
-                           "+20+20")]
-      (println "Resizing jpegs.")
-      (sh "mogrify" "-gravity" "NorthWest" "-extent" max-size "-background" "white"
-          "-border" "20x20" "-bordercolor" "white" "-colorspace" "RGB" file-name-wildcard))
+  (let [file-name-wildcard "images-jpeg/model-*.jpg"
+        file-sizes (:out (sh "identify" "-format" "%w %h\n" file-name-wildcard))
+        max-size (:out (sh "awk" "($1>w){w=$1} ($2>h){h=$2} END{print w\"x\"h}" :in file-sizes))
+        size-string (str (str/trim-newline max-size)
+                         "+20+20")]
+    (println "Resizing jpegs.")
+    (sh "mogrify" "-gravity" "NorthWest" "-extent" max-size "-background" "white"
+        "-border" "20x20" "-bordercolor" "white" "-colorspace" "RGB" file-name-wildcard))
 
   ;; Deals with program hanging due to futures.
   ;; https://clojuredocs.org/clojure.java.shell/sh
+
   (System/exit 0))
 
+;;---------------------------
 
-(-main)
+(comment
+
+  (def ensemble (slurp "raw-data/ensemble.txt"))
+
+  (let [svg-file "images-svg/ensemble.svg"
+        jpg-file "images-jpeg/ensemble.jpg"]
+    (spit svg-file (clygments/highlight ensemble :js :svg {:style "xcode"}))
+    (sh "svgexport" svg-file jpg-file "pad" "2x" "100%")))
+
+;;---------------------------
+
+(comment
+
+  (-main))
