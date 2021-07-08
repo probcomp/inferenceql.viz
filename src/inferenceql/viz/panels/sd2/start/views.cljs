51b463b4d2db85143622b8f3d9a735f4c193e8d1
Harish Tella <htella@mit.edu>
2021-07-08 11:57:07 -0400

    wip2

--- src/inferenceql/viz/panels/sd2/start/views.cljs
+++ src/inferenceql/viz/panels/sd2/start/views.cljs
@@ -4,7 +4,7 @@
             [inferenceql.viz.panels.sd2.start.db :refer [plot-data]]
             [inferenceql.viz.panels.viz.views :as viz]))
 
-(.log js/console :aoeu plot-data)
+;;(.log js/console :aoeu plot-data)
 
 (defn time-series [data]
   {:$schema "https://vega.github.io/schema/vega-lite/v5.json",
@@ -20,7 +20,7 @@
    :layer [{:mark "line"
             :encoding {:x {:field "time", :type "quantitative"
                            :axis {:grid true
-                                  :values (range 0 50 8)}}
+                                  :values (range 0 50 16)}}
                        :y {:field "expr-level", :type "quantitative"
                            :axis {:grid true
                                   :values (range 0 1.70 0.4)}}
@@ -39,6 +39,38 @@
                      :encoding {:text {:field "gene" :type "nominal"}}}]}]})
 
 
+(defn time-series-2 [data]
+  {:$schema "https://vega.github.io/schema/vega-lite/v5.json",
+   :width 1000
+   :config {:view {:stroke nil}}
+   :height 1000
+   :data {:values data}
+   ;;:transform [{:calculate "datum.status == 'rec' ? 1 : 0" :as "zorder"}]
+   :mark {:type "line" :tooltip {:content "data"}}
+   :encoding {:color {:field "status", :type "nominal"
+                      :scale {:domain ["rec", "not-rec"] :range ["#4e79a7" "#f28e2b"]}}}
+   :resolve {:axis {:x "independent" :y "independent"}}
+   :layer [{:mark "line"
+            :encoding {:x {:field "time", :type "quantitative"
+                           :axis {:grid true
+                                  :values (range 0 50 16)}}
+                       :y {:field "expr-level", :type "quantitative"
+                           :axis {:grid true
+                                  :values (range 0 1.70 0.4)}}
+                       ;;:scale {:domain [0.90, 1.4]}}
+                       :strokeDash {:field "gene" :type "nominal" :legend nil}}}
+           {:encoding {:x {:aggregate "max"
+                           :field "time"
+                           :type "quantitative"
+                           :axis nil}
+                       :y {:aggregate {:argmax "time"}
+                           :field "expr-level"
+                           :type "quantitative"
+                           :axis nil}}
+            :layer [{:mark {:type "circle"}
+                     :encoding {:detail {:field "gene" :type "nominal"}}}
+                    {:mark {:type "text" :align "left" :dx 4}
+                     :encoding {:text {:field "gene" :type "nominal"}}}]}]})
 
 
 
