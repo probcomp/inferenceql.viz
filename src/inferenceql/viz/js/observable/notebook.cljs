363d3cbe2932c1721f0a010f92dd371d35e91b0b
Harish Tella <htella@mit.edu>
2021-07-30 14:27:38 -0500

    feat: add basic structure for Observable comps

---
+++ src/inferenceql/viz/js/observable/notebook.cljs
@@ -0,0 +1,5 @@
+(ns inferenceql.viz.js.observable.notebook
+  "Build target for compiling a js-bundle for use of from an Observable notebook."
+  (:require [inferenceql.query.js]
+            [inferenceql.inference.js]
+            [inferenceql.viz.js.util]))
