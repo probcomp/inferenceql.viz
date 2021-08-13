a4cdd061adf2745e7d862c5517a2830190e70f47
Harish Tella <htella@mit.edu>
2021-08-13 17:01:55 -0500

    add working plot function

--- src/inferenceql/viz/js/comp.cljs
+++ src/inferenceql/viz/js/comp.cljs
@@ -1,12 +1,14 @@
 (ns inferenceql.viz.js.comp
   (:require [inferenceql.viz.js.components.plot.core
-             :refer [plot] :rename {plot plot-comp}]
+             :refer [plot circle-plot] :rename {plot plot-comp
+                                                circle-plot circle-plot-comp}]
             [inferenceql.viz.js.components.table.core
              :refer [table] :rename {table table-comp}]
             [inferenceql.viz.js.components.mini-app.core
              :refer [mini-app] :rename {mini-app mini-app-comp}]))
 
 (def ^:export plot plot-comp)
+(def ^:export circle-plot circle-plot-comp)
 
 (def ^:export table table-comp)
 
--- src/inferenceql/viz/js/components/plot/core.cljs
+++ src/inferenceql/viz/js/components/plot/core.cljs
@@ -5,7 +5,8 @@
             [clojure.walk :refer [postwalk]]
             [inferenceql.viz.js.components.plot.views :refer [vega-lite]]
             [inferenceql.viz.js.components.plot.vega :refer [generate-spec]]
-            [inferenceql.viz.js.util :refer [clj-schema]]))
+            [inferenceql.viz.js.util :refer [clj-schema]]
+            [inferenceql.viz.panels.viz.circle :refer [circle-viz-spec]]))
 
 (defn plot
   "Javascript interface to plot UI component. Returns a DOM node with a plot."
@@ -19,5 +20,19 @@
 
           node (dom/createElement "div")
           component [vega-lite spec {:actions false} nil nil]]
+      (rdom/render component node)
+      node)))
+
+(defn circle-plot
+  "Javascript interface to circle plot UI component. Returns a DOM node with a circle plot."
+  [node-names edges]
+  (if (and node-names edges)
+    (let [node-names (map keyword (->clj node-names))
+          edges (for [e (->clj edges)]
+                  (map keyword e))
+
+          spec (circle-viz-spec node-names edges)
+          node (dom/createElement "div")
+          component [vega-lite spec {:actions false :mode "vega"} nil nil]]
       (rdom/render component node)
       node)))
