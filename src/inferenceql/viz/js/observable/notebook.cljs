5cfc4e85269855d77ed2e110be8a57bb0a1fd110
Harish Tella <htella@mit.edu>
2021-09-07 18:57:53 -0500

    Breaking change: updates version of automodeling.

--- src/inferenceql/viz/js/observable/notebook.cljs
+++ src/inferenceql/viz/js/observable/notebook.cljs
@@ -2,7 +2,7 @@
   "Build target for compiling a js-bundle for use of from an Observable notebook."
   (:require [inferenceql.query.js]
             [inferenceql.inference.js]
-            [inferenceql.auto-modeling.xcat]
+            [inferenceql.auto-modeling.js]
             [inferenceql.viz.js.comp]
             [inferenceql.viz.js.util]
             [inferenceql.viz.js.smart.core]
