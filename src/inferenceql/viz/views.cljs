f587e6fd63bbc18a662e348f98f9c444b7a16430
Harish Tella <htella@mit.edu>
2021-09-08 17:40:41 -0500

    Get circle plot working

--- src/inferenceql/viz/views.cljs
+++ src/inferenceql/viz/views.cljs
@@ -1,6 +1,6 @@
 (ns inferenceql.viz.views
   (:require [re-frame.core :as rf]
-            [re-com.core :refer [v-box]]
+            [re-com.core :refer [v-box h-box gap]]
             [inferenceql.viz.config :refer [config]]
             [inferenceql.viz.panels.learning.views :as learning]
             [medley.core :as medley]
@@ -8,7 +8,10 @@
             [inferenceql.viz.panels.jsmodel.multimix :as multimix]
             [cljstache.core :refer [render]]
             [inferenceql.auto-modeling.js :refer [import-cgpm]]
-            [inferenceql.viz.panels.jsmodel.views :refer [js-code-block]]))
+            [inferenceql.viz.panels.jsmodel.views :refer [js-code-block]]
+            [inferenceql.viz.js.components.plot.views :refer [vega-lite]]
+            [inferenceql.viz.panels.viz.circle :refer [circle-viz-spec]]
+            [clojure.math.combinatorics :refer [combinations]]))
 
 ;;;; Views are expressed in Hiccup-like syntax. See the Reagent docs for more info.
 
@@ -29,8 +32,22 @@
                                 (:schema config)) ; TODO: better to use the schema in the db.
         mmix-model (crosscat/xcat->mmix xcat-model)
         js-model-text (render (:js-model-template config)
-                              (multimix/template-data mmix-model))]
+                              (multimix/template-data mmix-model))
+
+
+        node-names (map keyword (get cgpm-model "names"))
+        view-assignment (fn [col]
+                          (let [col-to-num (zipmap node-names (get cgpm-model "outputs"))
+                                col-num-to-view-num (into {} (get cgpm-model "Zv"))]
+                            (-> col col-to-num col-num-to-view-num)))
+        views (vals (group-by view-assignment node-names))
+        edges (mapcat #(combinations % 2) views)
+        circle-spec (circle-viz-spec node-names edges)]
     [v-box
+     :margin "20px"
      :children [[learning/panel]
-                [js-code-block js-model-text]
-                #_[viz/vega-lite vega-lite-spec {:actions false} generators pts-store]]]))
+                [gap :size "10px"]
+                [h-box
+                 :children [[vega-lite circle-spec {:actions false :mode "vega"} nil nil]
+                            [gap :size "20px"]
+                            [js-code-block js-model-text]]]]]))
