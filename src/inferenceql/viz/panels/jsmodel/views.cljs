4eedffe0bbf5e0c47c774cce124b6fa3214edc96
Harish Tella <htella@mit.edu>
2021-09-20 17:42:53 -0500

    wip

--- src/inferenceql/viz/panels/jsmodel/views.cljs
+++ src/inferenceql/viz/panels/jsmodel/views.cljs
@@ -25,21 +25,6 @@
                     (first))
         p-zero-tree (hickory.zip/hiccup-zip p-zero)
 
-
-        #_pp #_(->> (hickory.core/parse-fragment highlighted-js-text)
-                    (map hickory.core/as-hickory)
-                    first)
-        #_ppp #_(hickory.render/hickory-to-html pp)
-        #_nodes #_(s/select (s/and (s/class "hljs-keyword")
-                                   (s/tag :span)
-                                   (s/find-in-text #"if"))
-                            pp)
-
-        #_nodes #_(loop [n p-zero-tree]
-                    (if (not (zip/end? n))
-                      (do (.log js/console (zip/node n))
-                          (recur (zip/next n)))))
-
         ;; Removes n nodes. Returns position before all removals (depth-first).
         remove-n (fn [loc num-to-remove]
                    (loop [l loc n num-to-remove]
@@ -75,23 +60,11 @@
                        (zip/replace loc (string/replace node #"&quot;" "\""))
 
                        :else
-                       loc)))
-
-        fixed (loop [loc p-zero-tree]
-                (if (not (zip/end? loc))
-                  (recur (zip/next (fix-node loc)))
-                  (zip/root loc)))]
-
-    (.log js/console :p-zero p-zero)
-    (.log js/console :fixed fixed)
-
-    #_(.log js/console :orig highlighted-js-text)
-    #_(.log js/console :pp pp)
-    #_(.log js/console :ppp ppp)
-    #_(.log js/console (map hickory.core/as-hiccup (hickory.core/parse-fragment "&lArr;")))
-    #_(.log js/console (map hickory.core/as-hiccup (hickory.core/parse-fragment "&nbsp;")))
-    #_(.log js/console (map hickory.core/as-hiccup (hickory.core/parse-fragment "&quot;")))
-    fixed))
+                       loc)))]
+    (loop [loc p-zero-tree]
+      (if (not (zip/end? loc))
+        (recur (zip/next (fix-node loc)))
+        (zip/root loc)))))
 
 (defn js-code-block
   "Display of Javascript code with syntax highlighting.
