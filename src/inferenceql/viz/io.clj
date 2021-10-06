6e20e54279c685501e03c7d61e2a1fee91d6201c
Harish Tella <htella@mit.edu>
2021-10-06 18:46:20 -0500

    refactor: delete unused

--- src/inferenceql/viz/io.clj
+++
@@ -1,7 +0,0 @@
-(ns inferenceql.viz.io
-  (:require [clojure.java.io :as io]))
-
-(defmacro inline-resource
-  "Inlines the contents of the named resource as a string."
-  [n]
-  (slurp (io/resource n)))
