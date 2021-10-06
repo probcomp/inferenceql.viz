e4a981815ec7ac31e9ca1be1ef5d1062646a5b50
Harish Tella <htella@mit.edu>
2021-10-06 18:36:22 -0500

    refactor: remove unused parts of db

--- src/inferenceql/viz/db.cljs
+++ src/inferenceql/viz/db.cljs
@@ -1,24 +1,10 @@
 (ns inferenceql.viz.db
   (:require [clojure.spec.alpha :as s]
-            [inferenceql.viz.panels.control.db :as control-panel]
-            [inferenceql.viz.panels.override.db :as override-panel]
-            [inferenceql.viz.panels.table.db :as table-panel]
-            [inferenceql.viz.panels.viz.db :as viz-panel]
-            [inferenceql.viz.panels.more.db :as more-panel]
-            [inferenceql.viz.panels.modal.db :as modal-panel]
-            [inferenceql.viz.components.query.db :as query-component]
             [inferenceql.viz.components.store.db :as store-component]))
 
 ;;; Primary DB spec.
 
-(s/def ::db (s/keys :req-un [::control-panel/control-panel
-                             ::override-panel/override-panel
-                             ::table-panel/table-panel
-                             ::viz-panel/viz-panel
-                             ::more-panel/more-panel
-                             ::modal-panel/modal-panel
-                             ::query-component/query-component
-                             ::store-component/store-component]))
+(s/def ::db (s/keys :req-un [::store-component/store-component]))
 
 (def default-general-db
   "This db map is meant to contain keys and values not specific to
@@ -31,12 +17,5 @@
   and panel specific dbs all merged together."
   []
   (let [dbs [default-general-db
-             control-panel/default-db
-             override-panel/default-db
-             table-panel/default-db
-             viz-panel/default-db
-             more-panel/default-db
-             modal-panel/default-db
-             query-component/default-db
              store-component/default-db]]
     (apply merge dbs)))
