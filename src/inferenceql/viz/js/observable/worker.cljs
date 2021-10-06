49512561a263442d96f7eb151b4bb69d98d7b90c
Harish Tella <htella@mit.edu>
2021-10-06 18:02:51 -0500

    refactor: delete unused

--- src/inferenceql/viz/js/observable/worker.cljs
+++
@@ -1,5 +0,0 @@
-(ns inferenceql.viz.js.observable.worker
-  "Build target for compiling a js-bundle for use from a web worker in an Observable notebook."
-  (:require [inferenceql.query.js]
-            [inferenceql.inference.js]
-            [inferenceql.viz.js.util]))
