566713c77ec12c7bf625060b03c2e8cb454933aa
Harish Tella <htella@mit.edu>
2021-10-06 18:05:49 -0500

    refactor: delete unused

--- src/inferenceql/viz/panels/modal/subs.cljs
+++
@@ -1,9 +0,0 @@
-(ns inferenceql.viz.panels.modal.subs
-  (:require [re-frame.core :as rf]))
-
-(defn ^:sub content
-  "Returns the hiccup to be displayed as the content of the modal panel."
-  [db _]
-  (get-in db [:modal-panel :content]))
-(rf/reg-sub :modal/content
-            content)
--- src/inferenceql/viz/panels/more/subs.cljs
+++
@@ -1,9 +0,0 @@
-(ns inferenceql.viz.panels.more.subs
-  (:require [re-frame.core :as rf]))
-
-(defn ^:sub show-menu
-  "Returns whether we should show the more menu."
-  [db _]
-  (get-in db [:more-panel :show-menu]))
-(rf/reg-sub :more/show-menu
-            show-menu)
