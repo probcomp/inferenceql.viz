65a71459e274001d475f34c583a322a02208f29c
Harish Tella <htella@mit.edu>
2021-10-07 15:34:24 -0500

    remove more

--- src/inferenceql/viz/components/store/db.cljs
+++ src/inferenceql/viz/components/store/db.cljs
@@ -1,9 +1,5 @@
 (ns inferenceql.viz.components.store.db
-  "Contains the initial state of the db corresponding to the store component
-  The store component is esentially a part of the app-db where datasets and
-  models and geodata are stored."
-  (:require [clojure.spec.alpha :as s]
-            [inferenceql.viz.config :refer [config transitions]]
+  (:require [inferenceql.viz.config :refer [config transitions]]
             [inferenceql.viz.csv :refer [clean-csv-maps]]
             [inferenceql.viz.util :refer [keywordize-kv]]
             [inferenceql.inference.gpm.crosscat :as crosscat]
@@ -64,39 +60,3 @@
                        (map add-null-columns)
                        (map merge iteration-tags)))
 (def all-samples (concat observed-samples virtual-samples))
-
-
-;;; Setting up store component db
-
-(def default-db
-  "Portions of the initial store-db related to the compiled-in model and dataset.
-
-  The compiled-in dataset is called: data.
-  The compiled-in model is called: model."
-  {:store-component {:datasets {:data {:rows rows
-                                       :schema schema}}}})
-
-;;;  Specs for the store-db
-
-(s/def ::store-component (s/keys :req-un [::datasets]))
-
-;;; Specs for :datasets.
-
-(s/def ::datasets (s/map-of ::dataset-name ::dataset))
-(s/def ::dataset-name keyword?)
-(s/def ::dataset (s/keys :req-un [::rows
-                                  ::schema]))
-
-
-(s/def ::rows (s/coll-of ::row))
-(s/def ::row (s/map-of ::column-name any?))
-(s/def ::schema (s/map-of ::column-name ::stat-type))
-(s/def ::stat-type #{:numerical :nominal})
-(s/def ::column-name keyword?)
-
-;;; Accessor functions to portions of the table-panel db.
-
-(defn datasets
-  [db]
-  (get-in db [:store-component :datasets]))
-
--- src/inferenceql/viz/db.cljs
+++ src/inferenceql/viz/db.cljs
@@ -1,18 +1,15 @@
 (ns inferenceql.viz.db
   (:require [clojure.spec.alpha :as s]
-            [inferenceql.viz.components.store.db :as store-component]
             [inferenceql.viz.panels.control.db :as control-panel]))
 
 ;;; Primary DB spec.
 
-(s/def ::db (s/keys :req-un [::store-component/store-component
-                             ::control-panel/control-panel]))
+(s/def ::db (s/keys :req-un [::control-panel/control-panel]))
 
 (defn default-db
   "When the application starts, this will be the value put in `app-db`.
   It consists of keys and values from the general db
   and panel specific dbs all merged together."
   []
-  (let [dbs [store-component/default-db
-             control-panel/default-db]]
+  (let [dbs [control-panel/default-db]]
     (apply merge dbs)))
