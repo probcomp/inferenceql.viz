e026a883e598e6b095d839399888ada0caf3b609
Harish Tella <htella@mit.edu>
2021-10-07 15:30:50 -0500

    cleanup

--- src/inferenceql/viz/core.cljs
+++ src/inferenceql/viz/core.cljs
@@ -3,20 +3,14 @@
             [re-frame.core :as rf]
             [reagent.dom :as rdom]
             ;; Core
-            [inferenceql.viz.util :refer [query-string-params]]
             [inferenceql.viz.events]
             [inferenceql.viz.effects]
             [inferenceql.viz.views :as views]
-            [inferenceql.viz.vega :as vega.init]
-            ;; Store Component
-            [inferenceql.viz.components.store.subs]
             ;; Control
             [inferenceql.viz.panels.control.eventsubs]))
 
 (enable-console-print!)
 (set! *warn-on-infer* true)
-
-(vega.init/add-custom-vega-color-schemes)
 
 (defn ^:after-load render-app
   "Renders the primary reagent component for the app onto the DOM element, #app
@@ -33,7 +27,4 @@
   ;; We only initialize the app-db on first load. This is so figwheel's hot code reloading does
   ;; not reset the state of the app.
   (rf/dispatch-sync [:app/initialize-db])
-
-  (let [params (query-string-params)])
-
   (render-app))
--- src/inferenceql/viz/db.cljs
+++ src/inferenceql/viz/db.cljs
@@ -3,23 +3,16 @@
             [inferenceql.viz.components.store.db :as store-component]
             [inferenceql.viz.panels.control.db :as control-panel]))
 
-
 ;;; Primary DB spec.
 
 (s/def ::db (s/keys :req-un [::store-component/store-component
                              ::control-panel/control-panel]))
-
-(def default-general-db
-  "This db map is meant to contain keys and values not specific to
-  any application panel."
-  {})
 
 (defn default-db
   "When the application starts, this will be the value put in `app-db`.
   It consists of keys and values from the general db
   and panel specific dbs all merged together."
   []
-  (let [dbs [default-general-db
-             store-component/default-db
+  (let [dbs [store-component/default-db
              control-panel/default-db]]
     (apply merge dbs)))
