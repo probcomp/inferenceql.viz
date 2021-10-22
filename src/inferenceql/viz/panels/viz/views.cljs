5134a6a1c44721b3d1ece1948d584f2801b19d07
Harish Tella <htella@mit.edu>
2021-10-22 15:18:25 -0400

    wip

--- src/inferenceql/viz/panels/viz/views.cljs
+++ src/inferenceql/viz/panels/viz/views.cljs
@@ -31,14 +31,17 @@
         dom-nodes (r/atom {})
         vega-embed-result (r/atom nil)
 
-        free-resources (fn [] (swap! run inc) ; Turn off any running generators.
-                              (when @vega-embed-result
-                                ;; Free resources used by vega-embed.
-                                ;; See https://github.com/vega/vega-embed#api-reference
-                                (.finalize @vega-embed-result)))
+        free-resources (fn [teardown-fn]
+                         (swap! run inc) ; Turn off any running generators.
+                         (when @vega-embed-result
+                           ;; Run the user-provided teardown first.
+                           (when teardown-fn
+                             (teardown-fn @vega-embed-result))
 
-        ;; Used to set the pts-store whenever the mouse click is lifted.
-        mouseup-handler (fn [] (rf/dispatch [:viz/set-pts-store]))
+                           ;; Free resources used by vega-embed.
+                           ;; See https://github.com/vega/vega-embed#api-reference
+                           (.finalize @vega-embed-result)))
+
 
         ;; Uses generator functions in map `generators` to generate new rows and
         ;; insert them into `vega-instance`.
@@ -53,9 +56,9 @@
                                         (change (name dataset-name) changeset)
                                         (resize)
                                         (run))))))
-        embed (fn [this spec opt generators pts-store init-fn]
+        embed (fn [this spec opt generators pts-store options]
                 (if-not (:vega-node @dom-nodes)
-                  (free-resources)
+                  (free-resources (:teardown-fn options))
                   (let [spec (clj->js spec)
                         opt (clj->js (merge default-vega-embed-options
                                             opt))]
@@ -63,7 +66,8 @@
                                            spec
                                            opt)
                       (.then (fn [res]
-                               (when init-fn (init-fn res))))
+                               (when-let [init-fn (:init-fn options)]
+                                 (init-fn res))))
                       ;; Store the result of vega-embed.
                       (.then (fn [res]
                                (reset! vega-embed-result res)))
@@ -74,11 +78,7 @@
 
       :component-did-mount
       (fn [this]
-        (embed this spec opt generators pts-store (:init-fn options))
-        ;; Add global listener for mouseup.
-
-
-        (.addEventListener js/window "mouseup" mouseup-handler))
+        (embed this spec opt generators pts-store options))
 
       :component-did-update
       (fn [this old-argv]
@@ -88,20 +88,63 @@
           ;; We do not want to update when it is just `pts-store` that changed.
           (when (not= [old-spec old-opt old-generators]
                       [new-spec new-opt new-generators])
-            (embed this new-spec new-opt new-generators current-pts-store (:init-fn new-options)))))
+            (embed this new-spec new-opt new-generators current-pts-store new-options))))
 
       :component-will-unmount
       (fn [this]
-        (free-resources)
-
-        ;; Remove global listener for mouseup.
-        (.removeEventListener js/window "mouseup" mouseup-handler))
+        (let [[_ spec opt generators pts-store options] (r/argv this)]
+          (free-resources (:teardown-fn options))))
 
       :reagent-render
       (fn [spec opt generators pts-store options]
         (when spec
           [:div#viz-container
            [:div {:ref #(swap! dom-nodes assoc :vega-node %)}]]))})))
+
+
+(defn vega-lite-2
+  "vega-lite reagent component"
+  [spec opt generators pts-store]
+  (let [
+        ;; Used to set the pts-store whenever the mouse click is lifted.
+        mouseup-handler (fn [] (rf/dispatch [:viz/set-pts-store]))
+
+        ;; Update value of pts_store and attach a listener to it.
+        init-fn (fn [res]
+                 (let [view-obj (.-view res)
+                       spec-has-pts-store (try (some? (.data view-obj "pts_store"))
+                                               (catch :default e false))]
+                   (when spec-has-pts-store
+                     ;; Update value of pts_store in view object to the last value
+                     ;; we had saved.
+                     (when pts-store
+                       (.data view-obj "pts_store" (clj->js pts-store))
+                       (.run view-obj))
+
+                     ;; Listen to future updates to pts_store that come from interactions
+                     ;; in the visualization itself. Stage changes.
+                     (.addDataListener view-obj "pts_store"
+                                       (fn [_ds-name data]
+                                         (rf/dispatch [:viz/stage-pts-store data])))
+
+                     ;; Used to set the pts-store shortly after the mouse wheel is
+                     ;; done scrolling on a selection rectangle (pts_brush) within
+                     ;; a vega-lite visualization.
+                     (.addEventListener view-obj "wheel"
+                                        (gfn/debounce
+                                         (fn [_event item]
+                                           (when (= "pts_brush" (.. item -mark -name))
+                                             (rf/dispatch [:viz/set-pts-store])))
+                                         500)))))]
+    {:component-did-mount (fn [this]
+                            ;; Add global listener for mouseup.
+                            (.addEventListener js/window "mouseup" mouseup-handler))
+     :component-will-unmount (fn [this]
+                               ;; Remove global listener for mouseup.
+                               (.removeEventListener js/window "mouseup" mouseup-handler))
+     :reagent-render (fn [spec opt generators pts-store]
+                       [vega-lite spec opt generators pts-store {:init-fn init-fn}])}))
+
 
 
 
@@ -117,31 +160,3 @@
                     (js/requestAnimationFrame send))))))))
 
 
-;; Update value of pts_store and attach a listener to it.
-;; TODO: move this to a vega-embed init function in a new reagent component.
-#_(.then (fn [res]
-           (let [view-obj (.-view res)
-                 spec-has-pts-store (try (some? (.data view-obj "pts_store"))
-                                         (catch :default e false))]
-             (when spec-has-pts-store
-               ;; Update value of pts_store in view object to the last value
-               ;; we had saved.
-               (when pts-store
-                 (.data view-obj "pts_store" (clj->js pts-store))
-                 (.run view-obj))
-
-               ;; Listen to future updates to pts_store that come from interactions
-               ;; in the visualization itself. Stage changes.
-               (.addDataListener view-obj "pts_store"
-                                 (fn [_ds-name data]
-                                   (rf/dispatch [:viz/stage-pts-store data])))
-
-               ;; Used to set the pts-store shortly after the mouse wheel is
-               ;; done scrolling on a selection rectangle (pts_brush) within
-               ;; a vega-lite visualization.
-               (.addEventListener view-obj "wheel"
-                                  (gfn/debounce
-                                   (fn [_event item]
-                                     (when (= "pts_brush" (.. item -mark -name))
-                                       (rf/dispatch [:viz/set-pts-store])))
-                                   500))))))
